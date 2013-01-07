499a36fa85e7ddcf3403531f9e2ec6c1045cd5d4
bfanti <bfanti@peaksware.com>
2013-01-07 11:09:06 -0700

    Initial commit of Backbone Boilerplate scaffolding.

---
+++ vendor/jam/require.config.js
@@ -0,0 +1,223 @@
+var jam = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "Backbone.Marionette",
+            "location": "../vendor/jam/Backbone.Marionette",
+            "main": "lib/amd/backbone.marionette.js"
+        },
+        {
+            "name": "Backbone.Marionette.Handlebars",
+            "location": "../vendor/jam/Backbone.Marionette.Handlebars",
+            "main": "backbone.marionette.handlebars.js"
+        },
+        {
+            "name": "handlebars",
+            "location": "../vendor/jam/handlebars",
+            "main": "handlebars.js"
+        },
+        {
+            "name": "hbt",
+            "location": "../vendor/jam/hbt",
+            "main": "hbt.js"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "text",
+            "location": "../vendor/jam/text",
+            "main": "text.js"
+        },
+        {
+            "name": "underscore",
+            "location": "../vendor/jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "version": "0.2.12",
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        }
+    }
+};
+
+if (typeof require !== "undefined" && require.config) {
+    require.config({
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "Backbone.Marionette",
+            "location": "../vendor/jam/Backbone.Marionette",
+            "main": "lib/amd/backbone.marionette.js"
+        },
+        {
+            "name": "Backbone.Marionette.Handlebars",
+            "location": "../vendor/jam/Backbone.Marionette.Handlebars",
+            "main": "backbone.marionette.handlebars.js"
+        },
+        {
+            "name": "handlebars",
+            "location": "../vendor/jam/handlebars",
+            "main": "handlebars.js"
+        },
+        {
+            "name": "hbt",
+            "location": "../vendor/jam/hbt",
+            "main": "hbt.js"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "text",
+            "location": "../vendor/jam/text",
+            "main": "text.js"
+        },
+        {
+            "name": "underscore",
+            "location": "../vendor/jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        }
+    }
+});
+}
+else {
+    var require = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "Backbone.Marionette",
+            "location": "../vendor/jam/Backbone.Marionette",
+            "main": "lib/amd/backbone.marionette.js"
+        },
+        {
+            "name": "Backbone.Marionette.Handlebars",
+            "location": "../vendor/jam/Backbone.Marionette.Handlebars",
+            "main": "backbone.marionette.handlebars.js"
+        },
+        {
+            "name": "handlebars",
+            "location": "../vendor/jam/handlebars",
+            "main": "handlebars.js"
+        },
+        {
+            "name": "hbt",
+            "location": "../vendor/jam/hbt",
+            "main": "hbt.js"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "text",
+            "location": "../vendor/jam/text",
+            "main": "text.js"
+        },
+        {
+            "name": "underscore",
+            "location": "../vendor/jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        }
+    }
+};
+}
+
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = jam;
+}
