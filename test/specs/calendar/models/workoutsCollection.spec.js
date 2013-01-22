568406ee3754c1d1750710ca7b1277cbca6328ad
Micah Condon <mcondon@peaksware.com>
2013-01-22 11:48:39 -0700

    added tests for workoutscollection

--- test/specs/calendar/models/workoutsCollection.spec.js
+++ test/specs/calendar/models/workoutsCollection.spec.js
@@ -8,14 +8,32 @@ describe("Workouts Collection spec", function()
 
         // use requirejs() here, not define(), for jasmine compatibility
         requirejs(
-        ["models/workoutsCollection"],
-        function(WorkoutsCollection)
+        ["moment",
+        "app",
+        "models/workoutsCollection"],
+        function(moment, theApp, WorkoutsCollection)
         {
             describe("Workouts Collection", function()
             {
                 it("should load as a module", function()
                 {
                     expect(WorkoutsCollection).toBeDefined();
+                });
+
+                describe("url", function()
+                {
+                    it("Should point to the correct api server url", function()
+                    {
+                        var startDate = moment().subtract("days", 30);
+                        var endDate = moment().add("days", 30);
+                        var dateFormat = "YYYY-MM-DD";
+                        var expectedUrl = "/WebApiServer/Fitness/V1/workouts/" +
+                            startDate.format(dateFormat) + "/" +
+                            endDate.format(dateFormat);
+                        var workouts = new WorkoutsCollection([], { startDate: startDate, endDate: endDate });
+                        expect(workouts.url()).toContain(expectedUrl);
+                        expect(workouts.url()).toContain(theApp.apiRoot);
+                    });
                 });
 
             });
