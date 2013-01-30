c493153ac01adeb894be221d09cd3f155aaed0e0
bfanti <bfanti@peaksware.com>
2013-01-30 10:40:31 -0700

    Add spec for Router unit testing Add CustomCalendarView and template, wip.

---
+++ app/scripts/views/customCalendarView.js
@@ -0,0 +1,45 @@
+﻿define(
+[
+    "backbone.marionette",
+    "hbs!templates/views/customCalendar"
+],
+function(Marionette, customCalendarTemplate)
+{
+    return Marionette.ItemView.extend(
+    {
+        template:
+        {
+            type: "handlebars",
+            template: customCalendarTemplate
+        },
+
+        modelEvents:
+        {
+            "change": "render"
+        },
+
+        collectionEvents:
+        {
+            //"add": "",
+            //"remove": "",
+            "reset": "render"
+        },
+
+        serializeData: function()
+        {
+            var data = this.model.toJSON();
+            _.extend(data, { collection: this.collection.toJSON });
+            return data;
+        },
+        
+        onRender: function()
+        {
+            if (!this.collection)
+                throw "CalendarView needs a Collection!";
+
+            var data = this.collection.toJSON();
+            
+        }
+
+    });
+});
