16867c9493f65b988515acea3b0b84404e72d8ac
Micah Condon <mcondon@peaksware.com>
2013-02-05 12:25:27 -0700

    beginnings of client events api

---
+++ app/scripts/models/clientEvent.js
@@ -0,0 +1,28 @@
+﻿define(
+[
+    "moment",
+    "TP"
+],
+function (moment, TP)
+{
+    return TP.APIModel.extend(
+    {
+
+        webAPIModelName: "ClientEvent",
+
+        // client events have no id - create only, no updates
+        validateIdAttribute: function(){},
+
+        // get person id from session
+        defaults: function()
+        {
+            return {
+                DateCreated: moment().unix(),
+                PersonId: theMarsApp.session.has("PersonId") ? theMarsApp.session.get("PersonId") : 0,
+                Host: null,
+                Event: null
+            };
+        }
+
+    });
+});
---
+++ app/scripts/models/clientEventsCollection.js
@@ -0,0 +1,18 @@
+﻿define(
+[
+    "TP",
+    "models/clientEvent"
+],
+function (TP, ClientEvent)
+{
+    return TP.Collection.extend(
+    {
+        model: ClientEvent,
+
+        url: function()
+        {
+            return theMarsApp.apiRoot + "/ClientEvents/V1/Event";
+        }
+
+    });
+});
