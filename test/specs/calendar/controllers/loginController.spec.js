5d31953a9bf152fc0007442fbef7f5ff1c628112
Aram Malinich <amalinich@peaksware.com>
2013-02-07 11:26:29 -0700

    added tests around loginController after refactoring details going to clientEvent Model

---
+++ test/specs/calendar/controllers/loginController.spec.js
@@ -0,0 +1,31 @@
+﻿// use requirejs() here, not define(), for jasmine compatibility
+requirejs(
+[
+    "underscore",
+    "app",
+    "controllers/loginController"
+],
+function (_, theApp, LoginController) {
+
+    describe("The Login Controller", function () {
+
+        var triggerSpy;
+
+        beforeEach(function () {
+            triggerSpy = jasmine.createSpyObj("Trigger spy", ["trigger"]);
+        });
+
+        it("Should trigger an event login success", function () {
+            LoginController.prototype.onLoginSuccess.call(triggerSpy);
+            expect(triggerSpy.trigger).toHaveBeenCalledWith("login:success");
+        });
+
+        it("Should log a client event on login success", function () {
+            spyOn(theApp.clientEvents, "logEvent");
+            LoginController.prototype.onLoginSuccess.call(triggerSpy);
+            expect(theApp.clientEvents.logEvent).toHaveBeenCalledWith({ Event: { Type: "Login", Label: "Login", AppContext: "Login" } });
+        });
+
+    });
+
+});
