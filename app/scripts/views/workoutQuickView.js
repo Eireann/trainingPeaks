b47bc07c640d8361a61f137821b4690a10551566
mbrady <mbrady@peaksware.com>
2013-02-12 11:52:51 -0700

    US22 adding qorkout QV

---
+++ app/scripts/views/workoutQuickView.js
@@ -0,0 +1,43 @@
+﻿define(
+[
+    "TP",
+    "jqueryui/dialog",
+    "hbs!templates/views/workoutQuickView"
+
+],
+function(TP, dialog, workoutQuickView)
+{
+    "use strict";
+
+    return TP.ItemView.extend(
+    {
+        template:
+        {
+            type: "handlebars",
+            template: workoutQuickView
+        },
+
+        ui:
+        {
+        },
+        
+        events:
+        {
+        },
+
+        onBeforeRender: function ()
+        {
+            this.$el.dialog(
+            {
+                autoOpen: false,
+                modal:true
+            });
+        },
+
+        onRender: function ()
+        {
+            this.$el.dialog("open");
+        }
+
+    });
+});
