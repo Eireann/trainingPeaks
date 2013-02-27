2e9446afd7ca60bb926c2ea20fd12435aa3c6f79
Micah Condon <mcondon@peaksware.com>
2013-02-27 13:03:19 -0700

    working on drag and drop tests

--- test/selenium_extensions/betterDragAndDrop.js
+++ test/selenium_extensions/betterDragAndDrop.js
@@ -1,12 +1,25 @@
 Selenium.prototype.doBetterDragAndDrop = function(fromLocator, toLocator) {
-    var fromEl = window.$(fromLocator.replace("css=","")).offset();
-    var toEl = window.$(toLocator.replace("css=","")).offset();
-    if(fromLocator.indexOf("css=") !== 0) {
-    	fromLocator = "css=" + fromLocator;
-    }
-    var diffX = toEl.left - fromEl.left;
-    var diffY = toEl.top - fromEl.top;
+   
+    fromLocator = fromLocator.replace("css=","");
+    toLocator = toLocator.replace("css=","");
+
+    this.doEcho(fromLocator);
+    this.doEcho(toLocator);
+
+	for(var key in this) {
+	this.doEcho(key);
+	}
+	this.doEcho(this.window);
+	this.doEcho(this.window.$);
+    this.doEcho(window.$(fromLocator));
+    var fromOffset = window.$(fromLocator).offset();
+    var toOffset = window.$(toLocator).offset();
+
+    this.doEcho(fromOffset);
+    this.doEcho(toOffset);
+    var diffX = toOffset.left - fromOffset.left;
+    var diffY = toOffset.top - fromOffset.top;
     var coords = diffX + "," + diffY;
     this.doEcho("Dragging by " + coords);
-    this.doDragAndDrop(fromLocator, coords);
+    this.doDragAndDrop("css=" + fromLocator, coords);
 }
