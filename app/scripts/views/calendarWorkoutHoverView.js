991fc1e5e60c5d97f66c110bbf8499a532288d4b
Micah Condon <mcondon@peaksware.com>
2013-03-04 15:07:21 -0700

    US263 css cleanup

--- app/scripts/views/calendarWorkoutHoverView.js
+++ app/scripts/views/calendarWorkoutHoverView.js
@@ -40,8 +40,10 @@ function(TP, CalendarWorkoutHoverTemplate)
         onRender: function()
         {
             $('body').append(this.$el);
-            this.$el.css("left", this.posX - 20).css("top", this.posY - this.$el.height());
-        }
+            // git rid of the overflow that was caused by css triangle and shadows, or else it messes up our mouseover
+            this.$el.height(this.$('.details').height());
+            this.$el.css("left", this.posX - 20).css("top", this.posY - this.$el.height() - 50);
+        },
 
     });
 });
