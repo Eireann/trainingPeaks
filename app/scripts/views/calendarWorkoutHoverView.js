d32c010a15b9dd705ae8969feebabaa9f05e5499
Micah Condon <mcondon@peaksware.com>
2013-03-04 15:09:06 -0700

    US263 fixed jslint error

--- app/scripts/views/calendarWorkoutHoverView.js
+++ app/scripts/views/calendarWorkoutHoverView.js
@@ -43,7 +43,7 @@ function(TP, CalendarWorkoutHoverTemplate)
             // git rid of the overflow that was caused by css triangle and shadows, or else it messes up our mouseover
             this.$el.height(this.$('.details').height());
             this.$el.css("left", this.posX - 20).css("top", this.posY - this.$el.height() - 50);
-        },
+        }
 
     });
 });
