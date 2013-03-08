2c04447d34bcf112d7f8a668d77546c3b12f2023
Aram Malinich <amalinich@peaksware.com>
2013-03-08 11:46:57 -0700

    coverage and refactor of printKeyStat

--- test/specs/utilities/printKeyStat.spec.js
+++ test/specs/utilities/printKeyStat.spec.js
@@ -7,10 +7,10 @@ function(TP, printKeyStat)
 {
     describe("Display workout distance, time, or tss", function()
     {
-        it("Should display a backbone model with the precendent of workout distance, duration, and tss", function()
+        it("Should display a completed workout of backbone model with the precendent of distance, duration, and tss", function()
         {
             var workout = new TP.Model();
-            workout.set({ distance: 20, totalTime: 1, tssActual: 100 });
+            workout.set({ distance: 20, totalTime: 1, tssActual: 100, distancePlanned: 20, totalTimePlanned: 1, tssPlanned: 100 });
             expect(printKeyStat(workout)).toBe("0.01 mi");
 
             workout.set("distance", null);
@@ -20,22 +20,42 @@ function(TP, printKeyStat)
             expect(printKeyStat(workout)).toBe("100 tss");
             
             workout.set("tssActual", null);
+            expect(printKeyStat(workout)).toBe("0.01 mi");
+
+            workout.set("distancePlanned", null);
+            expect(printKeyStat(workout)).toBe("01:00");
+
+            workout.set("totalTimePlanned", null);
+            expect(printKeyStat(workout)).toBe("100 tss");
+
+            workout.set("tssPlanned", null);
+            expect(printKeyStat(workout)).toBe("");
+            
+
+        });
+        
+        xit("Should display a planned workout of backbone model with the precendent of distance, duration, and tss", function ()
+        {
+            var workout = new TP.Model();
+            workout.set({ distancePlanned: 20, totalTimePlanned: 1, tssPlanned: 100 });
+            expect(printKeyStat(workout)).toBe("0.01 mi");
+
+            workout.set("distancePlanned", null);
+            expect(printKeyStat(workout)).toBe("01:00");
+
+            workout.set("totalTimePlanned", null);
+            expect(printKeyStat(workout)).toBe("100 tss");
+
+            workout.set("tssPlanned", null);
             expect(printKeyStat(workout)).toBe("");
         });
         
-        it("Should display a JSON object with the precendent of workout distance, duration, and tss", function ()
+        it("Should accept a JSON object", function ()
         {
             var workout = { distance: 20, totalTime: 1, tssActual: 100 };
             expect(printKeyStat(workout)).toBe("0.01 mi");
-
-            workout.distance = null;
-            expect(printKeyStat(workout)).toBe("01:00");
-
-            workout.totalTime = null;
-            expect(printKeyStat(workout)).toBe("100 tss");
-
-            workout.tssActual = null;
-            expect(printKeyStat(workout)).toBe("");
         });
+        
+        
     });
 });
