83742939df29b744c6759fd3972db3a208cbe276
bfanti <bfanti@peaksware.com>
2013-03-12 16:14:32 -0600

    US410 WIP

---
+++ app/scripts/views/newItemView.js
@@ -0,0 +1,38 @@
+﻿define(
+[
+    "TP",
+    "jqueryui/dialog",
+    "hbs!templates/views/newItemView"
+],
+function (TP, dialog, newItemViewTemplate)
+{
+    return TP.ItemView.extend(
+    {
+        events:
+        {
+        },
+
+        template:
+        {
+            type: "handlebars",
+            template: newItemViewTemplate
+        },
+
+        onBeforeRender: function ()
+        {
+            this.$el.dialog(
+            {
+                autoOpen: false,
+                modal: true,
+                width: 800,
+                height: 600,
+                resizable: false
+            });
+        },
+
+        onRender: function ()
+        {
+            this.$el.dialog("open");
+        }
+    });
+});
