d3b15661de2002f0ccb376cdb343c55727a7d909
Aram Malinich <amalinich@peaksware.com>
2013-03-12 11:00:57 -0600

    US415:wired up delete functionality and created confirmation dialog

---
+++ app/scripts/views/deleteConfirmationView.js
@@ -0,0 +1,45 @@
+﻿define(
+[
+    "TP",
+    "hbs!templates/views/deleteConfirmationView"
+],
+function (TP, deleteConfirmationView)
+{
+    return TP.ItemView.extend(
+    {
+
+        showThrobbers: false,
+        tagName: "div",
+
+        events:
+        {
+            "click #deleteConfirmationDelete": "onDeleteConfirmed",
+            "click #deleteConfirmationCancel" : "onDeleteCancelled"
+        },
+
+        template:
+        {
+            type: "handlebars",
+            template: deleteConfirmationView
+        },
+
+        onRender: function ()
+        {
+            $('body').append(this.$el);
+            this.$el.dialog({
+                modal: true
+            });
+        },
+        
+        onDeleteConfirmed: function()
+        {
+            this.trigger("deleteConfirmed");
+            this.close();
+        },
+        
+        onDeleteCancelled: function()
+        {
+            this.close();
+        },
+    });
+});
