b71abc6818ad4b507daea11f10eea3bffb73d0b9
Aram Malinich <amalinich@peaksware.com>
2013-03-12 12:29:57 -0600

    US415:changed dialog buttons to yes and no

---
+++ test/specs/calendar/views/deleteConfirmationView.spec.js
@@ -0,0 +1,21 @@
+﻿// use requirejs() instead of define() here, to keep jasmine test runner happy
+requirejs(
+[
+    "jquery",
+    "views/deleteConfirmationView"
+],
+function ($, DeleteConfirmationView)
+{
+    describe("DeleteConfirmationView", function ()
+    {
+        it("Should close on cancel", function ()
+        {
+
+        });
+        
+        it("Should trigger event 'deleteConfirmed'", function()
+        {
+            
+        });
+    });
+});
