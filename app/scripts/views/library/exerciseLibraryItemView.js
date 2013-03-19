7fb7b083316feafd36c306260afdbe32468664ab
Micah Condon <mcondon@peaksware.com>
2013-03-19 12:35:48 -0600

    US414 working on drag UI

--- app/scripts/views/library/exerciseLibraryItemView.js
+++ app/scripts/views/library/exerciseLibraryItemView.js
@@ -2,15 +2,16 @@ define(
 [
     "TP",
     "jqueryui/draggable",
-    "hbs!templates/views/library/exerciseLibraryItemView"
+    "hbs!templates/views/library/exerciseLibraryItemView",
+    "hbs!templates/views/library/exerciseLibraryItemViewDragState"
 ],
-function(TP, draggable, ExerciseLibraryItemViewTemplate)
+function(TP, draggable, ExerciseLibraryItemViewTemplate, ExerciseLibraryItemViewTemplateDragState)
 {
     return TP.ItemView.extend(
     {
 
         tagName: "div",
-        className: "exercise",
+        className: "libraryExercise",
 
         attributes: function()
         {
@@ -38,10 +39,20 @@ function(TP, draggable, ExerciseLibraryItemViewTemplate)
 
         makeDraggable: function()
         {
+            _.bindAll(this, "draggableHelper");
             this.$el.data("ItemId", this.model.id);
             this.$el.data("ItemType", this.model.webAPIModelName);
             this.$el.data("DropEvent", "addExerciseFromLibrary");
-            this.$el.draggable({ appendTo: 'body', helper: 'clone', opacity: 0.7, 'z-index': 100 });
+            this.$el.draggable({ appendTo: 'body', 'z-index': 100, cursorAt: { top: 15, left: 25 }, helper: this.draggableHelper });
+            
+        },
+
+        draggableHelper: function()
+        {
+            console.log(this.model.attributes);
+            var $helperEl = $(ExerciseLibraryItemViewTemplateDragState(this.serializeData()));
+            $helperEl.addClass(this.className);
+            return $helperEl;
         }
 
     });
