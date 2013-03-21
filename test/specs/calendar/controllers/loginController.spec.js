094c74169ea6175673c4a353c05ed2cda96b4f40
Aram Malinich <amalinich@peaksware.com>
2013-03-21 09:48:59 -0600

    added test coverage

--- test/specs/calendar/controllers/loginController.spec.js
+++ test/specs/calendar/controllers/loginController.spec.js
@@ -5,27 +5,42 @@ requirejs(
     "app",
     "controllers/loginController"
 ],
-function (_, theApp, LoginController) {
+function (_, theApp, LoginController)
+{
 
-    describe("The Login Controller", function () {
+    describe("The Login Controller", function ()
+    {
 
         var triggerSpy;
 
-        beforeEach(function () {
+        beforeEach(function ()
+        {
             triggerSpy = jasmine.createSpyObj("Trigger spy", ["trigger"]);
         });
 
-        it("Should trigger an event login success", function () {
+        it("Should trigger an event login success", function ()
+        {
             LoginController.prototype.onLoginSuccess.call(triggerSpy);
             expect(triggerSpy.trigger).toHaveBeenCalledWith("login:success");
         });
 
-        it("Should log a client event on login success", function () {
+        it("Should log a client event on login success", function ()
+        {
             spyOn(theApp.clientEvents, "logEvent");
             LoginController.prototype.onLoginSuccess.call(triggerSpy);
             expect(theApp.clientEvents.logEvent).toHaveBeenCalledWith({ Event: { Type: "Login", Label: "Login", AppContext: "Login" } });
         });
 
-    });
+        it("Should attempt to navigate and reload on logout", function ()
+        {
+            spyOn(theApp.router, "navigate");
+            LoginController.prototype.onLogout.call();
+            expect(theApp.router.navigate).toHaveBeenCalledWith("login");
+        });
 
+        it("Should create a new loginview and subscribe to login success", function ()
+        {
+            LoginController.prototype.show.call();
+        });
+    });
 });
