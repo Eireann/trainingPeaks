726cee3395807f96f247a8b91fd16c1cd93d6364
Aram Malinich <amalinich@peaksware.com>
2013-04-01 10:40:09 -0600

    added in initial checkin of layout formatter

---
+++ app/scripts/utilities/workoutLayoutFormatter.js
@@ -0,0 +1,31 @@
+﻿define(
+[],
+function()
+{
+    var layoutDefinitions =
+    {
+        "0": "",
+        "1": "duration",
+        "2" : "distance",
+        "3" : "averageSpeed",
+        "4" : "averagePace",
+        "5" : "calories",
+        "6" : "elevationGain",
+        "7" : "elevationLoss",
+        "8" : "energy",
+        "9" : "TSS",
+        "10" : "IF",
+        "11" : "normalizedPower",
+        "12" : "normalizedPace",
+        "13" : "heartRate",
+        "14" : "power",
+        "15" : "torque",
+        "16" : "elevation",
+        "17" : "cadence",
+        "18" : "speed",
+        "19" : "pace",
+        "20" : "temperature",
+    };
+
+    return layoutDefinitions;
+});
