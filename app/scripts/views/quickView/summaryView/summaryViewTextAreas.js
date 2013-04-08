ec64b1113de5fea9a189367da87b3b4ba0f43eb1
Micah Condon <mcondon@peaksware.com>
2013-04-08 13:39:17 -0600

    Refactoring quickview into multiple files

---
+++ app/scripts/views/quickView/summaryView/summaryViewTextAreas.js
@@ -0,0 +1,44 @@
+﻿define(
+[
+    "jqueryTextAreaResize",
+    "setImmediate"
+],
+function(
+    textAreaResize,
+    setImmediate
+)
+{
+
+    var summaryViewTextAreas = {
+
+        initializeTextAreas: function()
+        {
+            this.on("render", this.onRenderTextAreas, this);
+        },
+
+        onRenderTextAreas: function()
+        {
+            if (!this.textAreasInitialized)
+            {
+                var self = this;
+                this.$("textarea").autosize();
+                setImmediate(function() { self.setTextAreaHeight(); });
+                this.textAreasInitialized = true;
+            }
+        },
+
+        setTextAreaHeight: function()
+        {
+            if (this.$("#descriptionInput").val())
+                this.$("#descriptionInput").height(this.$("#descriptionInput")[0].scrollHeight);
+
+            if (this.$("#preActivityCommentsInput").val())
+                this.$("#preActivityCommentsInput").height(this.$("#preActivityCommentsInput")[0].scrollHeight);
+
+            if (this.$("#descriptionInput").val())
+                this.$("#postActivityCommentsInput").height(this.$("#postActivityCommentsInput")[0].scrollHeight);
+        }
+    };
+
+    return summaryViewTextAreas;
+});
