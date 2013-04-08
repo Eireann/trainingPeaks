4de928491c465d810b09db09b5e36315c2e0f76a
Aram Malinich <amalinich@peaksware.com>
2013-04-08 09:57:12 -0600

    refactoring utilities

---
+++ app/scripts/utilities/conversion.js
@@ -0,0 +1,13 @@
+﻿define(
+[
+    "utilities/convertTimeHoursToDecimal",
+    "utilities/convertToModelUnits",
+    "utilities/convertToViewUnits"
+], function (convertTimeHoursToDecimal, convertToModelUnits, convertToViewUnits)
+{
+    return {
+        convertTimeHoursToDecimal: convertTimeHoursToDecimal,
+        convertToModelUnits: convertToModelUnits,
+        convertToViewUnits: convertToViewUnits
+    };
+});
