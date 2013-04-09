4c2a10abdab9c9a24e49f54b81633ad80f30318b
Micah Condon <mcondon@peaksware.com>
2013-04-09 09:43:37 -0600

    US10 TA723 detect color of header image

---
+++ app/scripts/utilities/image.js
@@ -0,0 +1,33 @@
+﻿define(
+[
+
+], function ()
+{
+    var imageUtils = {
+
+        getImageColorAtRightEdge: function(img)
+        {
+            xOffset = img.width;
+            yOffset = Math.round(img.height / 2);
+            return this.getImageColor(img, xOffset, yOffset);
+        },
+
+        getImageColor: function(img, xOffset, yOffset)
+        {
+            var canvas = this.createCanvasFromImage(img);
+            return canvas.getContext('2d').getImageData(xOffset, yOffset, 1, 1).data;
+        },
+
+        createCanvasFromImage: function(img)
+        {
+            var canvas = $('<canvas/>')[0];
+            canvas.width = img.width;
+            canvas.height = img.height;
+            canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+            return canvas;
+        },
+
+    };
+
+    return imageUtils;
+});
--- app/scripts/views/userControlsView.js
+++ app/scripts/views/userControlsView.js
@@ -2,10 +2,11 @@
 [
     "underscore",
     "TP",
+    "utilities/image",
     "views/accountMenuView",
     "hbs!templates/views/userControls"
 ],
-function (_, TP, AccountMenuView, userControlsTemplate)
+function (_, TP, imageUtils, AccountMenuView, userControlsTemplate)
 {
     return TP.ItemView.extend(
     {
@@ -29,7 +30,12 @@ function (_, TP, AccountMenuView, userControlsTemplate)
         {
             "change": "render"
         },
-        
+ 
+        initialize: function()
+        {
+            this.on("render", this.updateHeaderColorFromLogoOnLoad, this);
+        },
+
         onUsernameLabelClicked: function(e)
         {
             //document.getSelection().removeAllRanges();
@@ -37,6 +43,25 @@ function (_, TP, AccountMenuView, userControlsTemplate)
             var offset = $(e.currentTarget).offset();
             this.accountMenu = new AccountMenuView({model: this.model, parentEl: this.$el });
             this.accountMenu.render().top(offset.top + 18).left(offset.left);
+        },
+
+        updateHeaderColorFromLogoOnLoad: function()
+        {
+            // only for coaches / affiliates - how?
+            _.bindAll(this, "updateHeaderColorFromLogo");
+            this.$("#topLogo").on("load", this.updateHeaderColorFromLogo);
+        },
+
+        updateHeaderColorFromLogo: function()
+        {
+        
+            var logoImg = this.$("#topLogo")[0];
+            var colorData = imageUtils.getImageColorAtRightEdge(logoImg);
+            // we can't just join colorData because it's a Uint8ClampedArray
+            //var rgbaColor = "rgba(" + colorData[0] + "," + colorData[1] + "," + colorData[2] + "," + colorData[3] + ")";
+            var rgbColor = "rgba(" + colorData[0] + "," + colorData[1] + "," + colorData[2] + ")";
+            this.$("#userControlsBackground").css("background-color", rgbColor);
         }
+
     });
 });
