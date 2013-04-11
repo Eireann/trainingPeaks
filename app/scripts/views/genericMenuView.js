1f6b1b36a8506d26f754de04435351a452df70a4
Micah Condon <mcondon@peaksware.com>
2013-04-11 16:29:01 -0600

    US434 workout comments

--- app/scripts/views/genericMenuView.js
+++ app/scripts/views/genericMenuView.js
@@ -1,9 +1,9 @@
 define(
 [
     "TP",
-    "hbs!templates/views/genericMenuTemplate"
+    "hbs!templates/views/genericMenu"
 ],
-function (TP, genericMenuTemplate)
+function(TP, genericMenuTemplate)
 {
     return TP.ItemView.extend(
     {
@@ -11,64 +11,44 @@ function (TP, genericMenuTemplate)
         modal: true,
         showThrobbers: false,
         tagName: "div",
-        className: "workoutSettingsHover",
-
-        events:
-        {
-            "click #workoutSettingsLabelDelete": "onDelete",
-            "click #workoutSettingsLabelCopy": "onCopyClicked",
-            "click #workoutSettingsLabelCut": "onCutClicked"
-        },
-
-        onCopyClicked: function()
-        {
-            this.model.trigger("workout:copy", this.model);
-            this.close();
-        },
-        
-        onCutClicked: function()
-        {
-            this.model.trigger("workout:cut", this.model);
-            this.close();
-        },
-
-        hideWorkoutSettings: function (e)
-        {
-            this.close();
-            this.trigger("mouseleave", e);
-            //delete this;
-        },
-
-        initialize: function(options)
-        {
-            this.posX = options.left;
-            this.posY = options.top;
-            this.parentEl = options.parentEl;
-            this.inheritedClassNames = options.className;
-        },
-
-        attributes: function()
-        {
-            return {
-                "id": "workoutSettingsDiv",
-                "data-workoutId": this.model.id
-            };
-        },
 
         template:
         {
             type: "handlebars",
-            template: calendarWorkoutSettingsHover
+            template: genericMenuTemplate
         },
 
-        onDelete: function()
+        initialize: function(options)
         {
+
+            if (!options.labels || !options.labels.length)
+            {
+                throw "GenericMenuView requires a labels array";
+            }
+
+            this.model = new TP.Model({ labels: options.labels });
+
+            this.on("modalrender", this.addGenericClassName, this);
+        },
+
+        addGenericClassName: function()
+        {
+            // because css class name can be overridden in constructor options, but we still want the generic in there
+            this.$el.addClass("genericMenu");
+            this.$overlay.addClass("genericMenuOverlay");
+        },
+
+        events:
+        {
+            "click label": "onLabelClicked"
+        },
+
+        onLabelClicked: function(e)
+        {
+            var labelEventName = $(e.target).text();
+            this.trigger(labelEventName);
             this.close();
-            
-            this.deleteConfirmationView = new UserConfirmationView({ template: deleteConfirmationTemplate });
-            this.deleteConfirmationView.render();
-            var self = this;
-            this.deleteConfirmationView.on("userConfirmed", function () { self.model.destroy({ wait: true }); });
         }
+       
     });
 });
--- app/scripts/views/quickView/summaryView/workoutCommentView.js
+++ app/scripts/views/quickView/summaryView/workoutCommentView.js
@@ -1,9 +1,10 @@
 define(
 [
     "TP",
+    "views/genericMenuView",
     "hbs!templates/views/quickView/workoutComments"
 ],
-function(TP, WorkoutCommentsTemplate)
+function(TP, GenericMenuView, WorkoutCommentsTemplate)
 {
     return TP.ItemView.extend(
     {
@@ -11,10 +12,46 @@ function(TP, WorkoutCommentsTemplate)
         tagName: "div",
         className: "comment",
 
+        events:
+        {
+            "mouseenter": "showMenuButton",
+            "mouseleave": "hideMenuButton",
+            "click .menuButton": "showMenu"
+        },
+
         template:
         {
             type: "handlebars",
             template: WorkoutCommentsTemplate
+        },
+
+        showMenuButton: function()
+        {
+            this.$(".menuButton").show();
+        },
+
+        hideMenuButton: function()
+        {
+            this.$(".menuButton").hide();
+        },
+
+        showMenu: function(e)
+        {
+            var menuView = new GenericMenuView({ className: "workoutCommentMenu", labels: ['Copy', 'Delete'] });
+            menuView.on("Delete", this.onDeleteClicked, this);
+            menuView.on("Copy", this.onCopyClicked, this);
+            menuView.render().bottom(e.pageY).center(e.pageX);
+        },
+
+        onDeleteClicked: function()
+        {
+            alert('delete');
+        },
+
+        onCopyClicked: function()
+        {
+            alert('copy');
         }
+
     });
 });
