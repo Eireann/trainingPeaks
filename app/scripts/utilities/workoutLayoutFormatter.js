071b251937f6aa4e192e391d1779e131f820e787
Aram Malinich <amalinich@peaksware.com>
2013-04-12 08:54:44 -0600

    US442

--- app/scripts/utilities/workoutLayoutFormatter.js
+++ app/scripts/utilities/workoutLayoutFormatter.js
@@ -4,29 +4,29 @@ function ()
 {
     return {
         quickViewLayout:
-        [
-            false,
-            "duration",
-            "distance",
-            "averageSpeed",
-            "averagePace",
-            "calories",
-            "elevationGain",
-            "elevationLoss",
-            "energy",
-            "TSS",
-            "IF",
-            "normalizedPower",
-            "normalizedPace",
-            "heartRate",
-            "power",
-            "torque",
-            "elevation",
-            "cadence",
-            "speed",
-            "pace",
-            "temperature"
-        ],
+        {
+            "0": "",
+            "1": "duration",
+            "2": "distance",
+            "3": "averageSpeed",
+            "4": "averagePace",
+            "5": "calories",
+            "6": "elevationGain",
+            "7": "elevationLoss",
+            "8": "energy",
+            "9": "TSS",
+            "10": "IF",
+            "11": "normalizedPower",
+            "12": "normalizedPace",
+            "13": "heartRate",
+            "14": "power",
+            "15": "torque",
+            "16": "elevation",
+            "17": "cadence",
+            "18": "speed",
+            "19": "pace",
+            "20": "temperature"
+        },
         calendarWorkoutLayout:
         {
             "1": "completedStartTime",
