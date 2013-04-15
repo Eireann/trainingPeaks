82e484297ce86eff5a77b6299e628b6dc7cc5d54
Micah Condon <mcondon@peaksware.com>
2013-04-15 15:47:00 -0600

    DE149 refactored week detection to use Moment.isoWeek instead of Moment.week

--- app/scripts/utilities/dates.js
+++ app/scripts/utilities/dates.js
@@ -4,6 +4,7 @@
 {
     return {
 
+        // note - if we change user settings to allow sunday week start, we'll have to change this method to detect user settings ...
         isThisWeek: function(dateToCheck)
         {
             var today = moment();
