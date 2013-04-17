5e3b1fc9215c1fd2cbffe24572f858be90a8c127
Micah Condon <mcondon@peaksware.com>
2013-04-17 09:52:54 -0600

    US10 affiliate test fixes

--- test/specs/utilities/affiliates.spec.js
+++ test/specs/utilities/affiliates.spec.js
@@ -10,6 +10,7 @@ function($, theMarsApp, affiliateUtils)
     {
         it("Should recognize a trainingpeaks, non-coach, non-affiliate user account", function()
         {
+            theMarsApp.user.set("settings.account.isAthlete", true, { silent: true });
             theMarsApp.user.set("settings.account.isCoached", false, { silent: true });
             theMarsApp.user.set("settings.affiliate.code", "trainingpeaks3", { silent: true });
             theMarsApp.user.set("settings.account.headerImageUrl", "training_peaks_banner.png", { silent: true });
@@ -20,6 +21,7 @@ function($, theMarsApp, affiliateUtils)
 
         it("Should recognize a branded affiliate account", function()
         {
+            theMarsApp.user.set("settings.account.isAthlete", true, { silent: true });
             theMarsApp.user.set("settings.account.isCoached", false, { silent: true });
             theMarsApp.user.set("settings.affiliate.code", "runnersworld", { silent: true });
             theMarsApp.user.set("settings.account.headerImageUrl", "training_peaks_banner.png", { silent: true });
@@ -30,6 +32,7 @@ function($, theMarsApp, affiliateUtils)
 
         it("Should recognize a branded coach account", function()
         {
+            theMarsApp.user.set("settings.account.isAthlete", true, { silent: true });
             theMarsApp.user.set("settings.account.isCoached", true, { silent: true });
             expect(affiliateUtils.isTpAffiliate()).toBe(false);
             expect(affiliateUtils.isAffiliate()).toBe(false);
