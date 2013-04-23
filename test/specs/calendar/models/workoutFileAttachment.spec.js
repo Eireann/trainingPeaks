62c7bc8d5838efda6f376529afdc2c8a16ce8b57
mbrady <mbrady@peaksware.com>
2013-04-23 16:32:08 -0600

    workoutFileAttachment test

---
+++ test/specs/calendar/models/workoutFileAttachment.spec.js
@@ -0,0 +1,24 @@
+﻿requirejs(
+[
+    "TP",
+    "models/workoutFileAttachment"
+],
+function (TP, WorkoutFileAttachment)
+{
+    describe("Workout File Attachment ", function ()
+    {
+        it("should throw exception if there is no workout ID", function ()
+        {
+            var workoutFileAttachment = new WorkoutFileAttachment();
+            workoutFileAttachment.set("workoutId", null);
+
+            var getUrl = function ()
+            {
+                workoutFileAttachment.url();
+            };
+
+            expect(getUrl).toThrow();
+
+        });
+    });
+});
