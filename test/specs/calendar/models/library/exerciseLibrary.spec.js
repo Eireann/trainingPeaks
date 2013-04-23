5e282a88e35996e8ab2e53ca89709d01f04781df
mbrady <mbrady@peaksware.com>
2013-04-23 12:15:21 -0600

    exerciseLibrary test

---
+++ test/specs/calendar/models/library/exerciseLibrary.spec.js
@@ -0,0 +1,44 @@
+﻿// use requirejs() here, not define(), for jasmine compatibility
+requirejs(
+    ["models/library/exerciseLibrary"],
+function (ExerciseLibrary)
+{
+    describe("exercise library Model", function ()
+    {
+
+        it("Should load as a module", function ()
+        {
+            expect(ExerciseLibrary).toBeDefined();
+        });
+
+        it("intialize should create a new library exercise collection", function ()
+        {
+            var exerciseLibrary = new ExerciseLibrary();
+
+            expect(exerciseLibrary.exercises).toBeDefined();
+        });
+
+        it("should fetch library exercises", function ()
+        {
+            var exerciseLibrary = new ExerciseLibrary();
+
+            spyOn(exerciseLibrary.exercises, "fetch");
+            exerciseLibrary.fetchExercises();
+
+            expect(exerciseLibrary.exercises.fetch).toHaveBeenCalled();
+        });
+
+        it("should not fetch library exercises", function ()
+        {
+            var exerciseLibrary = new ExerciseLibrary();
+            exerciseLibrary.exercises.length = 2;
+
+            spyOn(exerciseLibrary.exercises, "fetch");
+            exerciseLibrary.fetchExercises();
+
+            expect(exerciseLibrary.exercises.fetch).not.toHaveBeenCalled();
+        });
+
+    });
+
+});
