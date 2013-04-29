48a67cd894d4e4ece761f504824edb1aa7ad526b
Micah Condon <mcondon@peaksware.com>
2013-04-29 13:28:02 -0600

    US540 display heart rate time in zones table

--- app/scripts/views/quickView/hrView.js
+++ app/scripts/views/quickView/hrView.js
@@ -1,9 +1,11 @@
 ﻿define(
 [
     "TP",
-    "hbs!templates/views/quickView/hrTabView"
+    "hbs!templates/views/quickView/heartRate/hrTabView",
+    "hbs!templates/views/quickView/heartRate/hrZoneRow",
+    "hbs!templates/views/quickView/heartRate/hrPeakRow"
 ],
-function (TP, hrTabTemplate)
+function(TP, hrTabTemplate, hrZoneRowTemplate, hrPeakRowTemplate)
 {
     return TP.ItemView.extend(
     {
@@ -15,6 +17,33 @@ function (TP, hrTabTemplate)
         {
             type: "handlebars",
             template: hrTabTemplate
+        },
+
+        ui:
+        {
+            "heartRateByZonesChart": "#heartRateByZonesChart",
+            "heartRatePeaksChart": "#heartRatePeaksChart",
+            "heartRateByZonesTable": "#heartRateByZonesTable",
+            "heartRatePeaksTable": "#heartRatePeaksTable"
+        },
+
+        onRender: function()
+        {
+            this.renderTimeInZones();
+            this.model.get("details").on("change:timeInHeartRateZones", this.renderTimeInZones, this);
+        },
+
+        renderTimeInZones: function()
+        {
+            var timeInZones = this.model.get("details").get("timeInHeartRateZones");
+            if (timeInZones)
+            {
+                var zonesHtml = hrZoneRowTemplate(timeInZones);
+                this.ui.heartRateByZonesTable.html(zonesHtml);
+            } else
+            {
+                this.ui.heartRateByZonesTable.html("");
+            }
         }
     });
 });
