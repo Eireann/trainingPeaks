f32bef338fa0c8c089563f0cc6527a346631bfed
bfanti <bfanti@peaksware.com>
2013-05-01 09:11:24 -0600

    US643 Fix elevation area graph for negative elevations

---
+++ test/specs/calendar/views/quickView/mapAndGraphView.spec.js
@@ -0,0 +1,20 @@
+﻿// use requirejs() instead of define() here, to keep jasmine test runner happy
+requirejs(
+[
+    "TP",
+    "views/quickView/mapAndGraphView"
+],
+function (TP, QuickViewMapAndGraphView)
+{
+    describe("QuickView Map And Graph View", function ()
+    {
+        beforeEach(function ()
+        {
+        });
+
+        it("has an initializer", function()
+        {
+        });
+
+    });
+});
