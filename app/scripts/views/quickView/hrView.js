c67ddd4c8a36f7eee2b7b4f25f4c1a2ebf7a7375
Micah Condon <mcondon@peaksware.com>
2013-05-08 15:33:04 -0600

    US642 refactor

--- app/scripts/views/quickView/hrView.js
+++ app/scripts/views/quickView/hrView.js
@@ -2,101 +2,20 @@
 [
     "underscore",
     "TP",
-    "views/quickView/qvTableAndGraphTabs/timeInZones",
-    "views/quickView/qvTableAndGraphTabs/peaks",
-    "views/quickView/qvTableAndGraphTabs/stickitBindings",
-    "hbs!templates/views/quickView/heartRate/hrTabView"
+    "views/quickView/qvZonesTabs/baseView"
 ],
 function(
     _,
     TP,
-    timeInZonesMixin,
-    peaksMixin,
-    stickitMixin,
-    hrTabTemplate
+    zonesViewBase
 )
 {
-    
     var hrViewBase = {
-
         metric: "HeartRate",
-
-        className: "quickViewHrTab",
-
-        showThrobbers: true,
-
-        template:
-        {
-            type: "handlebars",
-            template: hrTabTemplate
-        },
-
-        initialEvents: function()
-        {
-            this.model.off("change", this.render);
-        },
-
-        initialize: function(options)
-        {
-            this.workoutModel = options.workoutModel;
-            this.initializeTimeInZones();
-            this.initializePeaks();
-            this.initializeStickit();
-        },
-
-        onInitialRender: function()
-        {
-            this.off("render", this.onInitialRender, this);
-            this.watchForModelChanges();
-            this.on("close", this.stopWatchingModelChanges, this);
-        },
-
-        // only on full update from server, won't happen on every small stickit change
-        reRenderOnChange: function(model, updateData, options)
-        {
-            if (!options["alreadyRendered" + this.metric + "Tab"])
-            {
-                this.unstickit();
-                this.stickitInitialized = false;
-                this.render();
-                options["alreadyRendered" + this.metric + "Tab"] = true;
-            }
-        },
-
-        getZoneSettingsByWorkoutTypeId: function(zoneSettingName, workoutTypeId)
-        {
-            var athleteSettings = theMarsApp.user.getAthleteSettings();
-            if (!athleteSettings || !athleteSettings.has(zoneSettingName))
-                return null;
-
-            var hrZoneSettings = athleteSettings.get(zoneSettingName);
-            if (!hrZoneSettings || !hrZoneSettings.length)
-                return null;
-
-            var defaultWorkoutTypeId = 0;
-            var workoutTypeSettings = null;
-            var defaultWorkoutTypeSettings = null;
-
-            _.each(hrZoneSettings, function(zoneSet)
-            {
-                if (zoneSet.workoutTypeId === workoutTypeId)
-                    workoutTypeSettings = zoneSet;
-
-                if (zoneSet.workoutTypeId === defaultWorkoutTypeId)
-                    defaultWorkoutTypeSettings = zoneSet;
-
-            }, this);
-
-            if (!workoutTypeSettings)
-                workoutTypeSettings = defaultWorkoutTypeSettings;
-
-            return workoutTypeSettings;
-        }
-
+        graphTitle: "Heart Rate",
+        className: "quickViewHrTab"
     };
 
-    _.extend(hrViewBase, timeInZonesMixin);
-    _.extend(hrViewBase, peaksMixin);
-    _.extend(hrViewBase, stickitMixin);
+    _.extend(hrViewBase, zonesViewBase);
     return TP.ItemView.extend(hrViewBase);
 });
