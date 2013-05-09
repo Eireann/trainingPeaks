bab1e3beacd7f6122fccfc6dddd28df4a0ccc8bc
Micah Condon <mcondon@peaksware.com>
2013-05-09 16:05:09 -0600

    US665 tomahawk added buttons and checkboxes

--- app/scripts/views/quickView/qvMain/qvFileUploadMenuView.js
+++ app/scripts/views/quickView/qvMain/qvFileUploadMenuView.js
@@ -11,13 +11,16 @@ function(TP, WorkoutFileUploadMenuTemplate)
         modal: true,
         showThrobbers: false,
         tagName: "div",
+        deviceAgentUrl: "http://support.trainingpeaks.com/device-agent.aspx",
 
         className: "workoutFileUploadMenu",
 
         events:
         {
             "click #workoutFileUploadMenuBrowse": "onBrowseClicked",
-            "click #closeIcon": "close"
+            "click .deviceAgentButton": "onDeviceAgentClicked",
+            "click #closeIcon": "close",
+            "change .fileSelect": "selectFileCheckbox"
         },
 
         attributes: {
@@ -38,6 +41,37 @@ function(TP, WorkoutFileUploadMenuTemplate)
         initialize: function(options)
         {
             this.$el.addClass(options.direction);
+        },
+
+        onDeviceAgentClicked: function()
+        {
+            window.open(this.deviceAgentUrl);
+        },
+
+        serializeData: function()
+        {
+            var data = this.model.toJSON();
+
+            if (this.model.has("details"))
+            {
+                data.details = this.model.get("details").toJSON();
+            }
+
+            return data;
+        },
+
+        selectFileCheckbox: function(e)
+        {
+            var checkbox = $(e.target);
+            var fileDiv = checkbox.closest(".file");
+            if (checkbox.is(":checked"))
+            {
+                fileDiv.addClass("selected");
+            } else
+            {
+                fileDiv.removeClass("selected");
+            }
         }
+
     });
 });
