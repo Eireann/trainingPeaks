fe2d6541e37997270ac0873c1df772f3873cc788
Micah Condon <mcondon@peaksware.com>
2013-05-09 13:10:10 -0600

    US665 tomahawk

---
+++ app/scripts/views/quickView/qvMain/qvFileUploadMenuView.js
@@ -0,0 +1,42 @@
+define(
+[
+    "TP",
+    "hbs!templates/views/quickView/workoutFileUploadMenu"
+],
+function(TP, WorkoutFileUploadMenuTemplate)
+{
+    return TP.ItemView.extend(
+    {
+
+        modal: true,
+        showThrobbers: false,
+        tagName: "div",
+
+        className: "workoutFileUploadMenu",
+
+        events:
+        {
+            "click #workoutFileUploadMenuBrowse": "onBrowseClicked"
+        },
+
+        attributes: {
+            "id": "workoutFileUploadMenuDiv"
+        },
+
+        template:
+        {
+            type: "handlebars",
+            template: WorkoutFileUploadMenuTemplate
+        },
+
+        onBrowseClicked: function()
+        {
+            this.trigger("browseFile");
+        },
+
+        initialize: function(options)
+        {
+            this.$el.addClass(options.direction);
+        }
+    });
+});
