8c8b63c002f89c9d9130b596e498ed20333460fa
Micah Condon <mcondon@peaksware.com>
2013-05-16 12:24:33 -0600

    US697 finished template logic starting css

--- app/scripts/views/expando/statsView.js
+++ app/scripts/views/expando/statsView.js
@@ -21,17 +21,46 @@ function(
 
         serializeData: function()
         {
+            var lapData = this.getLapData();
+            this.calculateMovingTime(lapData);
+            this.findAvailableMinMaxAvgFields(lapData);
+            return lapData;
+        },
+
+        getLapData: function()
+        {
             var workoutData = this.model.toJSON();
-
-
             var detailData = this.model.get("detailData").toJSON();
             var lapData = detailData.totalStats;
-
             _.extend(workoutData, lapData);
-
-            workoutData.movingTime = workoutData.totalTime - workoutData.stoppedTime;
-
             return workoutData;
+        },
+
+        calculateMovingTime: function(lapData)
+        {
+            if (lapData.stoppedTime)
+                lapData.movingTime = lapData.totalTime - lapData.stoppedTime;
+        },
+
+        findAvailableMinMaxAvgFields: function(lapData)
+        {
+            if (lapData.minimumPower !== null || lapData.averagePower !== null || lapData.maximumPower !== null)
+                lapData.minMaxPower = true;
+
+            if (lapData.minimumSpeed !== null || lapData.averageSpeed !== null || lapData.maximumSpeed !== null)
+                lapData.minMaxSpeed = true;
+
+            if (lapData.minimumHeartRate !== null || lapData.averageHeartRate !== null || lapData.maximumHeartRate !== null)
+                lapData.minMaxHeartRate = true;
+
+            if (lapData.minimumCadence !== null || lapData.averageCadence !== null || lapData.maximumCadence !== null)
+                lapData.minMaxCadence = true;
+
+            if (lapData.elevationMinimum !== null || lapData.elevationAverage !== null || lapData.elevationMaximum !== null)
+                lapData.minMaxElevation = true;
+
+            if (lapData.tempAvg !== null || lapData.tempMax !== null || lapData.tempMin !== null)
+                lapData.minMaxTemp = true;
         }
 
     };
