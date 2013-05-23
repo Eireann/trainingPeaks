ab8f85f083566e5d1451500c1efd53b9c492c3c0
bfanti <bfanti@peaksware.com>
2013-05-23 14:25:10 -0600

    US711 Just a few small improvements per Gear's request

--- app/scripts/utilities/charting/defaultFlotOptions.js
+++ app/scripts/utilities/charting/defaultFlotOptions.js
@@ -59,8 +59,33 @@ function(TP, flotCustomToolTip)
                     min: 0,
                     tickFormatter: function(value, axis)
                     {
-                        var decimalHours = (value / (3600 * 1000)).toFixed(2);
+                        var decimalHours = (value / (3600 * 1000));
                         return TP.utils.datetime.format.decimalHoursAsTime(decimalHours, true, null);
+                    },
+                    ticks: function(axis)
+                    {
+                        var ticksArray = [];
+
+                        if (axis.tickSize <= 120000)
+                            axis.tickSize = 120000;
+                        else if (axis.tickSize <= 300000)
+                            axis.tickSize = 300000;
+                        else if (axis.tickSize <= 600000)
+                            axis.tickSize = 600000;
+                        else if (axis.tickSize <= 900000)
+                            axis.tickSize = 900000;
+                        else if (axis.tickSize <= 1200000)
+                            axis.tickSize = 1200000;
+                        else
+                            axis.tickSize -= (axis.tickSize % 1500000);
+                        
+                        var max = axis.datamax;
+                        while (max > 0)
+                        {
+                            ticksArray.push(axis.datamax - max);
+                            max -= axis.tickSize;
+                        }
+                        return ticksArray;
                     }
                 }
             ]
