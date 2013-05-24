bec380a21bc95b87d942daa287a83d722d5c7c32
Micah Condon <mcondon@peaksware.com>
2013-05-24 11:27:15 -0600

    DE258 paperclip

--- app/scripts/views/quickView/qvMain/qvFileUploads.js
+++ app/scripts/views/quickView/qvMain/qvFileUploads.js
@@ -43,6 +43,30 @@ function (
             _.bindAll(this, "onUploadDone", "onUploadFail");
             _.extend(this.ui, this.fileUploadUi);
             _.extend(this.events, this.fileUploadEvents);
+            this.watchForFileAttachments();
+        },
+
+        watchForFileAttachments: function()
+        {
+            this.model.get("details").on("change:attachmentFileInfos", this.onAttachmentsChange, this);
+            this.on("close", this.stopWatchingForFileAttachments, this);
+        },
+
+        stopWatchingForFileAttachments: function()
+        {
+            this.model.get("details").off("change:attachmentFileInfos", this.onAttachmentsChange, this);
+        },
+
+        onAttachmentsChange: function()
+        {
+            var attachments = this.model.get("details").get("attachmentFileInfos");
+            if (attachments && attachments.length)
+            {
+                this.$(".addAttachment").addClass("withAttachments");
+            } else
+            {
+                this.$(".addAttachment").removeClass("withAttachments");
+            }
         },
 
         onUploadFileClicked: function()
