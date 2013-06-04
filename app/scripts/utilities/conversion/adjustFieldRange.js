0497b5ce6c49ed26af680c83ed815a69774ee28a
Aram Malinich <amalinich@peaksware.com>
2013-06-04 09:25:45 -0600

    US683: added pace

--- app/scripts/utilities/conversion/adjustFieldRange.js
+++ app/scripts/utilities/conversion/adjustFieldRange.js
@@ -4,6 +4,7 @@
 function ()
 {
     var maxDuration = 99 + (59 / 60); //Duration is in decimal hours, limit is 99:59:00 currently
+    var maxPace = 99 + (59 / 60) + (59 / 3600); //99:59:59  
 
     var adjustFieldRange = function(value, fieldName)
     {
@@ -27,6 +28,17 @@ function ()
                         newValue = maxDuration;
                     }
                     break;
+                case "pace":
+                    if (value < 0)
+                    {
+                        newValue = 0;
+                    }
+                    else if (value > maxPace)
+                    {
+                        newValue = maxPace;
+                    }
+                    break;
+                    
             }
             return newValue;
         }
