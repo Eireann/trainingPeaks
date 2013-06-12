fb4735bbc955dc38d8906e39aa0fe3f036b61410
Micah Condon <mcondon@peaksware.com>
2013-06-12 11:14:29 -0600

    US813 colors

---
+++ test/specs/utilities/charting/chartColors.spec.js
@@ -0,0 +1,15 @@
+﻿requirejs(
+[
+    "utilities/charting/chartColors"
+],
+function(chartColors)
+{
+    describe("seriesColorByChannel", function()
+    {
+        it("should be a valid object", function()
+        {
+            expect(chartColors).toBeDefined();
+            expect(chartColors.seriesColorByChannel).toBeDefined();
+        });
+    });
+});
