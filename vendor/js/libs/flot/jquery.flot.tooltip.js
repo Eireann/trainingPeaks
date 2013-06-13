5f826e0300ce429f30071f70429abeedab398b41
mbrady <mbrady@peaksware.com>
2013-06-13 13:07:12 -0600

    US813 - uncommented out the code that hides the tooltip

--- vendor/js/libs/flot/jquery.flot.tooltip.js
+++ vendor/js/libs/flot/jquery.flot.tooltip.js
@@ -86,7 +86,7 @@
                     }
                 }
                 else {
-                   // $tip.hide().html('');
+                    $tip.hide().html('');
                 }
             });
 
