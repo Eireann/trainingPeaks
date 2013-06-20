cd27c6646e05954353fbc1e871e32acf5a6fc233
Micah Condon <mcondon@peaksware.com>
2013-06-20 09:50:47 -0600

    US906 refactored library views

--- app/scripts/views/dashboard/dashboardLibrary.js
+++ app/scripts/views/dashboard/dashboardLibrary.js
@@ -1,16 +1,16 @@
 ﻿define(
 [
     "TP",
+    "views/pageContainer/libraryContainerView",
     "hbs!templates/views/dashboard/dashboardLibrary"
 ],
-function(TP, dashboardLibraryTemplate)
+function(
+    TP,
+    LibraryContainerView,
+    dashboardLibraryTemplate)
 {
-    var DashboardLibraryView =
+    return LibraryContainerView.extend(
     {
-
-        activeLibraryName: "chartsLibrary",
-        widthClosed: 40,
-        widthOpen: 310,
 
         template:
         {
@@ -18,106 +18,28 @@ function(TP, dashboardLibraryTemplate)
             template: dashboardLibraryTemplate
         },
 
-        events:
+        initialize: function(options)
         {
-            //"click #tabs > div": "onTabClick"
-            "click #tabs #chartsLibrary": "onTabClick"
+            // initialize the superclass
+            this.constructor.__super__.initialize.call(this, options);
         },
 
-        initialize: function()
+        buildViews: function(options)
         {
-            _.bindAll(this, "resizeContainerHeight");
-            $(window).on("resize", this.resizeContainerHeight);
-        },
+            var ChartsLibraryView = TP.ItemView.extend(
+                {
+                    template:
+                    {
+                        type: "handlebars",
+                        template: function() { return "<div>Charts Library</div>"; }
+                    }
+                }
+            );
 
-        onTabClick: function(e)
-        {
-            this.toggleLibrary(e.target.id);
-        },
-
-        toggleLibrary: function(newLibraryName)
-        {
-            if (this.isOpen())
-            {
-                this.hideLibrary();
-            }
-            else
-            {
-                this.showLibrary();
-            }
-        },
-
- 
-        showLibrary: function()
-        {
-            this.$el.parent().removeClass("closed").addClass("open");
-            this.animate({ width: this.widthOpen });
-            this.turnOnTab(this.activeLibraryName);
-            this.resizeContainerHeight();
-        },
-
-        hideLibrary: function()
-        {
-            if (!this.isOpen())
-                return;
-
-            this.$el.parent().removeClass("open").addClass("closed");
-            this.animate({ width: this.widthClosed });
-            if (this.activeLibraryName)
-            {
-                this.turnOffTab(this.activeLibraryName);
-            }
-        },
-
-        animate: function(cssAttributes)
-        {
-            // allow the calendar or other listeners to hook into our animation
-            var duration = 300;
-            this.trigger("animate", cssAttributes, duration);
-
-            // run the animation
-            this.$el.closest("#libraryContainer").animate(cssAttributes, { duration: duration });
-        },
-
-        turnOffTab: function(tabName)
-        {
-            this.$("#tabs #" + tabName).removeClass("active");
-        },
-
-        turnOnTab: function(tabName)
-        {
-            this.$("#tabs #" + tabName).addClass("active");
-        },
-
-        isOpen: function()
-        {
-            return this.$el.parent().hasClass("open");
-        },
-
-        onRender: function()
-        {
-            this.resizeContainerHeight();
-        },
-
-        onShow: function()
-        {
-            this.resizeContainerHeight();
-        },
-
-        resizeContainerHeight: function(event)
-        {
-            var headerHeight = $("#navigation").height();
-            var windowHeight = $(window).height();
-            var libraryHeight = windowHeight - headerHeight - 75 + 'px';
-            if (this.$el)
-            {
-                this.$el.height(libraryHeight);
-            }
-            this.$("#tabs").css({ height: libraryHeight });
-            this.$("#activeLibraryContainer").css({ height: libraryHeight });
+            this.views = {
+                chartsLibrary: new ChartsLibraryView()
+            };
         }
 
-    };
-
-    return TP.ItemView.extend(DashboardLibraryView);
+    });
 });
