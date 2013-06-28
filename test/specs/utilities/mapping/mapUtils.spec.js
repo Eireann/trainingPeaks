5ce8aa815d342436a66b89ad9bec6054bd4113b3
Aram Malinich <amalinich@peaksware.com>
2013-06-28 10:59:44 -0600

    added some more unit tests

---
+++ test/specs/utilities/mapping/mapUtils.spec.js
@@ -0,0 +1,55 @@
+﻿requirejs(
+[
+    "utilities/mapping/mapUtils",
+    "testUtils/testHelpers"
+],
+function(mapUtils, testHelpers)
+{
+    describe("MapUtils", function()
+    {
+        beforeEach(function ()
+        {
+            testHelpers.startTheApp();
+        });
+
+        afterEach(function ()
+        {
+            testHelpers.stopTheApp();
+        });
+        
+        it("should calculate mile marker intervals", function()
+        {
+            var interval = mapUtils.calculateMileMarkerInterval(10, 10);
+            expect(interval.distanceBetweenMarkers).toBe(1000);
+            expect(interval.countBy).toBe(1);
+            
+            var interval = mapUtils.calculateMileMarkerInterval(30000, 20);
+            expect(interval.distanceBetweenMarkers).toBe(2000);
+            expect(interval.countBy).toBe(2);
+        });
+
+        it("should calculate mile markers", function()
+        {
+            var dataParser = {
+              getLatLonArray: function()
+              {
+                  return [[1,1],[2,2],[3,3],[4,4]];
+              },
+              dataByChannel: { Distance: [[0,0],[1000,1000],[2000,2000],[3000,3000]]}
+            };
+            spyOn(mapUtils, "calculateMileMarkerInterval").andReturn(
+                { distanceBetweenMarkers: 1000, countBy: 1 }
+            );
+
+            var markers = mapUtils.calculateMileMarkers(dataParser, null);
+            expect(markers.length).toBe(3);
+            for (var i = 0; i < markers.length; i++)
+            {
+                var marker = markers[i];
+                expect(marker.latLng).toEqual([i + 2, i + 2]);
+                expect(marker.options.title).toEqual((i + 1) + " km");
+            }
+            console.log(markers);
+        });
+    });
+});
