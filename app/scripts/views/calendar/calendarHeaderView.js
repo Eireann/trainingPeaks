3cc98e6f756a915210f1e6a0ac3ddbc941c3f2ef
mbrady <mbrady@peaksware.com>
2013-07-02 09:32:14 -0600

    US913 added ability for coach to switch between athlete calendars

--- app/scripts/views/calendar/calendarHeaderView.js
+++ app/scripts/views/calendar/calendarHeaderView.js
@@ -1,10 +1,11 @@
 define(
 [
     "TP",
+    "moment",
     "views/applicationHeader/coachAndAffiliateCustomizations",
     "hbs!templates/views/calendar/calendarHeader"
 ],
-function(TP, coachAndAffiliateCustomizations, calendarHeaderTemplate)
+function(TP, moment, coachAndAffiliateCustomizations, calendarHeaderTemplate)
 {
     var calendarHeaderViewBase = {
 
@@ -21,8 +22,66 @@ function(TP, coachAndAffiliateCustomizations, calendarHeaderTemplate)
             "click #goToTodayButton": "onGoToTodayButtonClicked",
             "click #goToNextWeekButton": "onGoToNextWeekButtonClicked",
             "click #goToLastWeekButton": "onGoToLastWeekButtonClicked",
-            "click button.refreshButton": "onRefreshButtonClicked"
+            "click button.refreshButton": "onRefreshButtonClicked",
+
+            "change #athleteCalendarSelect": "onAthleteSelectBoxChange"
+
         },
+
+        initializeStickit: function()
+        {
+            this.on("render", this.stickitOnRender, this);
+        },
+
+        stickitOnRender: function()
+        {
+            if (!this.stickitInitialized)
+            {
+                this.stickit();
+                this.stickitInitialized = true;
+            }
+        },
+
+        bindings:
+        {
+            ".headerMonth":
+            {
+                observe: "date",
+                onGet: "formatMonth"
+            },
+
+            ".headerYear":
+            {
+                observe: "date",
+                onGet: "formatYear"
+            },
+        },
+
+        formatMonth: function(value)
+        {
+            if (value)
+            {
+                return moment(value).format("MMMM");
+            }
+            else
+            {
+                return "";
+            }
+        },
+
+        formatYear: function(value)
+        {
+            if (value)
+            {
+                return moment(value).format("YYYY");
+            }
+            else
+            {
+                return "";
+            }
+        },
+
+        modelEvents: {},
 
         initialize: function()
         {
@@ -30,6 +89,49 @@ function(TP, coachAndAffiliateCustomizations, calendarHeaderTemplate)
                 throw "Cannot have a calendarHeaderView without a model";
 
             this.initializeCoachAndAffiliateCustomizations();
+            this.initializeStickit();
+        },
+
+        onRender: function ()
+        {
+            if (!theMarsApp.user.get("settings.account.isAthlete"))
+            {
+                this.customizeAthleteSelectBox();
+                var currentUserId = theMarsApp.user.getCurrentAthleteId();
+                this.$("#athleteCalendarSelect").val(currentUserId);
+            }
+        },
+
+        customizeAthleteSelectBox: function()
+        {
+            var self = this;
+            setImmediate(function ()
+            {
+                self.$("#athleteCalendarSelect").selectBoxIt({
+                    dynamicPositioning: false
+                });
+                self.$("#athleteCalendarSelectSelectBoxItContainer").css('display', "block");
+                
+            });
+
+
+        },
+
+        onAthleteSelectBoxChange: function ()
+        {
+            var athleteId = Number(this.$("#athleteCalendarSelect").val());
+
+            var athleteUrl = "calendar/athletes/" + athleteId;
+            theMarsApp.router.navigate(athleteUrl, true);
+            
+        },
+
+        serializeData: function ()
+        {
+            var headerData = this.model.toJSON();
+            headerData.athletes = theMarsApp.user.get("athletes");
+
+            return headerData;
         },
 
         onGoToTodayButtonClicked: function()
