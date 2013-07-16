e7f1d680ecf9e063eb0c6e7eb2577e98e3cf17a0
Aram Malinich <amalinich@peaksware.com>
2013-07-16 16:21:25 -0600

    US663: jank attempt

--- app/scripts/views/home/athlete/athleteHomeActivity.js
+++ app/scripts/views/home/athlete/athleteHomeActivity.js
@@ -2,12 +2,16 @@
 [
     "underscore",
     "TP",
+    "moment",
+    "models/workoutsCollection",
     "views/home/scrollableColumnView",
     "hbs!templates/views/home/athlete/athleteHomeActivity"
 ],
 function(
     _,
     TP,
+    moment,
+    WorkoutsCollection,
     ScrollableColumnView,
     activityTemplate
     )
@@ -19,6 +23,18 @@ function(
         {
             // initialize the superclass
             this.constructor.__super__.initialize.call(this, { template: activityTemplate });
+
+            var now = moment();
+
+            var workouts = new WorkoutsCollection([], { startDate: now.subtract('weeks', 2) , endDate: now.add('weeks', 3) });
+
+            workouts.fetch().done(this.fetchFinished);
+
+        },
+        
+        fetchFinished: function(e)
+        {
+            
         }
 
     });
