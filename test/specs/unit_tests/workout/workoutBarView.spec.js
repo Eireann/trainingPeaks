769d3c9b075227e3a8861a2e8f6703394e437dfb
Aram Malinich <amalinich@peaksware.com>
2013-07-16 12:58:05 -0600

    US663: adding tests

---
+++ test/specs/unit_tests/workout/workoutBarView.spec.js
@@ -0,0 +1,31 @@
+﻿requirejs(
+[
+    "moment",
+    "views/workout/workoutBarView",
+    "models/workoutModel"
+],
+function(moment, WorkoutBarView, WorkoutModel)
+{
+    describe("WorkoutBarView", function()
+    {
+        var workoutModel;
+        
+        beforeEach(function()
+        {
+            workoutModel = new WorkoutModel({
+                workoutValueTypeId: 1
+                , totalTime: 1.25
+                , totalTimePlanned: 1.5
+                , distance: 1000
+                , distancePlanned: 1200
+                , tssActual: 100
+                , tssPlanned: 110
+            });
+        });
+        
+        it("should go get me a pizza", function()
+        {
+
+        });
+    });
+});
