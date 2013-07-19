661f1695b854b093c7fc6d3e1ec526c3b9d1fec7
Micah Condon <mcondon@peaksware.com>
2013-07-19 10:00:25 -0600

    US664 created a library view

---
+++ app/scripts/views/calendar/library/trainingPlanLibraryView.js
@@ -0,0 +1,23 @@
+﻿define(
+[
+    "underscore",
+    "TP",
+    "hbs!templates/views/calendar/library/trainingPlanLibraryView"
+],
+function(
+    _,
+    TP,
+    trainingPlanLibraryViewTemplate)
+{
+    var TrainingPlanLibraryView = {
+
+        template:
+        {
+            type: "handlebars",
+            template: trainingPlanLibraryViewTemplate
+        }
+
+    };
+
+    return TP.ItemView.extend(TrainingPlanLibraryView);
+});
