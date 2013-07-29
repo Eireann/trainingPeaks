f657addbbb9b3c9daf2544932c458a378ecdb48a
Micah Condon <mcondon@peaksware.com>
2013-07-29 12:32:02 -0600

    US900 displaying all possible stat keys for a given workout

---
+++ app/scripts/helpers/ifHasKey.js
@@ -0,0 +1,22 @@
+﻿define(
+[
+    "handlebars",
+    "TP"
+],
+function (Handlebars, TP)
+{
+
+    var ifHasKey = function(key, context, options)
+    {
+        if (context.hasOwnProperty(key))
+        {
+            return options.fn(this);
+        } else
+        {
+            return options.inverse(this);
+        }
+    };
+
+    Handlebars.registerHelper("ifHasKey", ifHasKey);
+    return ifHasKey;
+});
