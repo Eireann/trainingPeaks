1fdde2692b0f64a871b24e7f0d0ab594d6e6e74c
Micah Condon <mcondon@peaksware.com>
2013-07-31 10:15:41 -0600

    Updated flot tooltip positions

--- app/scripts/utilities/charting/flotToolTipPositioner.js
+++ app/scripts/utilities/charting/flotToolTipPositioner.js
@@ -43,7 +43,7 @@ function()
 
             if (tooltipLocation.left + tooltipWidth > canvasLocation.left + canvasWidth - rightLimit)
             {
-                var tooltipLeft = tooltipLocation.left - tooltipWidth - 40;
+                var tooltipLeft = tooltipLocation.left - tooltipWidth - 60;
                 $tooltipEl.css("left", tooltipLeft + "px");
                 $tooltipEl.removeClass("right").addClass("left");
             }
