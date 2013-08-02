2c3610409e3afdd0745caa9904bb943fc6d30186
Micah Condon <mcondon@peaksware.com>
2013-08-02 06:28:07 -0600

    US899 fixed test

--- test/specs/bdd_tests/trainingPlans/applyTrainingPlan.spec.js
+++ test/specs/bdd_tests/trainingPlans/applyTrainingPlan.spec.js
@@ -339,6 +339,10 @@ function(
                 expect(testHelpers.hasRequest(null, "plans/v1/commands/applyplan")).toBe(false);
 
                 var thursday = moment().day(4);
+                if(thursday.format("YYYY-MM-DD") < moment().format("YYYY-MM-DD"))
+                {
+                    thursday.add("weeks", 1);
+                }
                 var tuesday = moment().day(2);
                 $body.find("#applyDateType").val("3");
                 $body.find("#applyDate").val(tuesday.format("M/D/YYYY"));
