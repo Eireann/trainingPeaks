ade361420e979483713d1a908ec2b3cdd7cb00ac
Micah Condon <mcondon@peaksware.com>
2013-08-08 15:35:32 -0600

    US940 added start angle

--- app/scripts/views/dashboard/fitnessSummaryChart.js
+++ app/scripts/views/dashboard/fitnessSummaryChart.js
@@ -69,7 +69,8 @@ function(
                 pie: 
                 {
                     show: true,
-                    layerSlices: true
+                    layerSlices: true,
+                    startAngle: 0.
                 }
             };
 
