1244108afefc5e89b6f13b3556a7fe1842e64292
Micah Condon <mcondon@peaksware.com>
2013-08-15 12:43:42 -0600

    US1376 mysterious errors

---
+++ test/specs/unit_tests/framework/dataManager.spec.js
@@ -0,0 +1,119 @@
+requirejs([
+    "TP",
+    "framework/dataManager"
+],
+function(
+    TP,
+    DataManager
+) {
+
+    describe("Data Manager", function()
+    {
+        var dataManager;
+
+        var FakeModel = TP.Model.extend({
+            url: "fakeModelUrl",
+            fetch: function(options)
+            {
+                this.fetchOptions = options;
+            },
+            resolve: function(data, options)
+            {
+                this.fetchOptions.success(this, data, options);
+            }
+        });
+
+        beforeEach(function()
+        {
+            dataManager = new DataManager({
+                resetPatterns: [/resetme/i]
+            });
+        });
+
+        it("Should resolve a request when the source request resolves", function()
+        {
+            var fakeModel = new FakeModel();
+            var deferred = dataManager.fetch(fakeModel);
+            expect(deferred.state()).toBe("pending");
+            fakeModel.resolve({ valueOne: "hello", valueTwo: "world" });
+            expect(deferred.state()).toBe("resolved");
+            expect(fakeModel.get("valueOne")).toBe("hello");
+            expect(fakeModel.get("valueTwo")).toBe("world");
+        });
+
+        it("Should resolve multiple pending requests with the same data", function()
+        {
+            var fakeModelOne = new FakeModel();
+            var fakeModelTwo = new FakeModel();
+            var deferredOne = dataManager.fetch(fakeModelOne);
+            var deferredTwo = dataManager.fetch(fakeModelTwo);
+            expect(deferredOne.state()).toBe("pending");
+            expect(deferredTwo.state()).toBe("pending");
+
+            fakeModelOne.resolve({ valueOne: "hello", valueTwo: "world" });
+
+            expect(deferredOne.state()).toBe("resolved");
+            expect(deferredTwo.state()).toBe("resolved");
+            expect(fakeModelTwo.get("valueOne")).toBe("hello");
+            expect(fakeModelTwo.get("valueTwo")).toBe("world");
+        });
+
+        it("Should resolve new request with previously resolved data", function()
+        {
+            var fakeModelOne = new FakeModel();
+            var deferredOne = dataManager.fetch(fakeModelOne);
+            expect(deferredOne.state()).toBe("pending");
+            fakeModelOne.resolve({ valueOne: "hello", valueTwo: "world" });
+            expect(deferredOne.state()).toBe("resolved");
+
+            var fakeModelTwo = new FakeModel();
+            var deferredTwo = dataManager.fetch(fakeModelTwo);
+            expect(deferredTwo.state()).toBe("resolved");
+            expect(fakeModelTwo.get("valueOne")).toBe("hello");
+            expect(fakeModelTwo.get("valueTwo")).toBe("world");
+        });
+
+        it("Should reset when a matching model is saved", function()
+        {
+            var saveModelUrl = "/fitness/v1/resetme/something/";
+            var fakeModelOne = new FakeModel();
+            dataManager.fetch(fakeModelOne);
+            fakeModelOne.resolve({ valueOne: "hello", valueTwo: "world" });
+
+            dataManager.reset(saveModelUrl); 
+            var fakeModelTwo = new FakeModel();
+            var deferredTwo = dataManager.fetch(fakeModelTwo);
+            expect(deferredTwo.state()).toBe("pending");
+        });
+
+        it("Should not reset when a non matching model is saved", function()
+        {
+            var saveModelUrl = "/fitness/v1/idontmatch/";
+            var fakeModelOne = new FakeModel();
+            dataManager.fetch(fakeModelOne);
+            fakeModelOne.resolve({ valueOne: "hello", valueTwo: "world" });
+
+            dataManager.reset(saveModelUrl); 
+            var fakeModelTwo = new FakeModel();
+            var deferredTwo = dataManager.fetch(fakeModelTwo);
+            expect(deferredTwo.state()).toBe("resolved");
+        });
+
+        describe("Post requests", function()
+        {
+            it("Should use the requestSignature attribute or method if it exists", function()
+            {
+                var fakeModelWithRegularUrl = new FakeModel();
+                var fakeModelWithRequestSignature = new FakeModel();
+                fakeModelWithRequestSignature.requestSignature = "iamanonstandardrequest";
+
+                var deferredOne = dataManager.fetch(fakeModelWithRegularUrl);
+                var deferredTwo = dataManager.fetch(fakeModelWithRequestSignature);
+                fakeModelWithRegularUrl.resolve({ something: "string" });
+                expect(deferredOne.state()).toBe("resolved");
+                expect(deferredTwo.state()).toBe("pending");
+            });
+        });
+    });
+
+});
