68836cba7e0ba845921356b3335c35e0116418fc
Micah Condon <mcondon@peaksware.com>
2013-08-28 16:00:09 -0600

    Disabled plato as it appears to be slowing down our build by several minutes

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,7 +322,8 @@ module.exports = function(grunt)
 
     // grunt build builds a single minified js for dev/uat/live, at build/release
     // grunt build_debug does the same but doesn't minify, and points to local dev config
-    grunt.registerTask("build_common", ["clean", "coverage", "requirejs_config", "i18n_config", "requirejs", "concat", "compass:build", "copy:build_common", "copy:build_coverage", "plato:build"]);
+    // removed "plato:build" as last step of build_common
+    grunt.registerTask("build_common", ["clean", "coverage", "requirejs_config", "i18n_config", "requirejs", "concat", "compass:build", "copy:build_common", "copy:build_coverage"]);
     grunt.registerTask("build_debug", ["build_common", "copy:build_debug", "targethtml:build_debug", "copy-i18n-files"]);
     grunt.registerTask("build_debug_fast", ["clean", "requirejs_config", "requirejs", "concat", "compass:build", "copy:build_common", "copy:build_coverage", "copy:build_debug", "targethtml:build_debug"]);
     grunt.registerTask("build", ["build_common", "copy:build", "uglify", "deleteFiles:build", "targethtml:build", "copy-i18n-files"]);
