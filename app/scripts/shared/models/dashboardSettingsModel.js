eb0be798b88d802c173d6cfee8580bf52b6c8c07
Micah Condon <mcondon@peaksware.com>
2013-09-04 12:41:47 -0600

    US1369 moved dashboard settings save from user save to it's own endpoint

---
+++ app/scripts/shared/models/dashboardSettingsModel.js
@@ -0,0 +1,28 @@
+﻿define(
+[
+    "TP"
+],
+function(TP)
+{
+    return TP.DeepModel.extend(
+    {
+        // easiest way to get backbone to PUT instead of POST
+        isNew: function()
+        {
+            return false;
+        },
+
+        defaults:
+        {
+            dateOptions: null,
+            pods: null
+        },
+
+        url: function()
+        {
+            var dashboardSettingsUrl = "users/v1/user/" + theMarsApp.user.id + "/settings/dashboard";
+            return theMarsApp.apiRoot + "/" + dashboardSettingsUrl;
+        }
+
+    });
+});
