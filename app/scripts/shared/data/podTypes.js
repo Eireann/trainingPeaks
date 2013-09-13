ba637ce9675e07ff5eccd302930b9aa5b256ccb0
Micah Condon <mcondon@peaksware.com>
2013-09-13 11:06:28 -0600

    US560 working on dashboard pods access

--- app/scripts/shared/data/podTypes.js
+++ app/scripts/shared/data/podTypes.js
@@ -9,132 +9,132 @@ function(
 
     var podTypes = [
         {
-        	podId: 1,
-        	podAccessString: "pod_meal_macronutrients_over_time"
+            podId: 1,
+            podAccessString: "pod_meal_macronutrients_over_time"
         },
         {
-        	podId: 2,
-        	podAccessString: "pod_calories_difference_over_time"
+            podId: 2,
+            podAccessString: "pod_calories_difference_over_time"
         },
         {
-        	podId: 3,
-        	podAccessString: "pod_workout_summary_over_time"
+            podId: 3,
+            podAccessString: "pod_workout_summary_over_time"
         },
         {
-        	podId: 4,
-        	podAccessString: "pod_health_vault_sync"
+            podId: 4,
+            podAccessString: "pod_health_vault_sync"
         },
         {
-        	podId: 8,
-        	podAccessString: "pod_criticalPowerBests"
+            podId: 8,
+            podAccessString: "pod_criticalPowerBests"
         },
         {
-        	podId: 10,
-        	podAccessString: "pod_WorkoutSummaryReportDuration"
+            podId: 10,
+            podAccessString: "pod_WorkoutSummaryReportDuration"
         },
         {
-        	podId: 11,
-        	podAccessString: "pod_WorkoutSummaryReportDistance"
+            podId: 11,
+            podAccessString: "pod_WorkoutSummaryReportDistance"
         },
         {
-        	podId: 12,
-        	podAccessString: "pod_RouteReporting"
+            podId: 12,
+            podAccessString: "pod_RouteReporting"
         },
         {
-        	podId: 13,
-        	podAccessString: "pod_DailyMetricsReporting"
+            podId: 13,
+            podAccessString: "pod_DailyMetricsReporting"
         },
         {
-        	podId: 14,
-        	podAccessString: "pod_ShoppingList"
+            podId: 14,
+            podAccessString: "pod_ShoppingList"
         },
         {
-        	podId: 15,
-        	podAccessString: "pod_DashboardWelcome"
+            podId: 15,
+            podAccessString: "pod_DashboardWelcome"
         },
         {
-        	podId: 16,
-        	podAccessString: "pod_RaceReports"
+            podId: 16,
+            podAccessString: "pod_RaceReports"
         },
         {
-        	podId: 17,
-        	podAccessString: "pod_TimeInHeartRateZonesByDate"
+            podId: 17,
+            podAccessString: "pod_TimeInHeartRateZonesByDate"
         },
         {
-        	podId: 18,
-        	podAccessString: "pod_TimeInHeartRateZonesByWeek"
+            podId: 18,
+            podAccessString: "pod_TimeInHeartRateZonesByWeek"
         },
         {
-        	podId: 19,
-        	podAccessString: "pod_LongestWorkoutByWeekDistance"
+            podId: 19,
+            podAccessString: "pod_LongestWorkoutByWeekDistance"
         },
         {
-        	podId: 20,
-        	podAccessString: "pod_LongestWorkoutByWeekDuration"
+            podId: 20,
+            podAccessString: "pod_LongestWorkoutByWeekDuration"
         },
         {
-        	podId: 21,
-        	podAccessString: "pod_KilojoulesByWeek"
+            podId: 21,
+            podAccessString: "pod_KilojoulesByWeek"
         },
         {
-        	podId: 22,
-        	podAccessString: "pod_MicronutrientReport"
+            podId: 22,
+            podAccessString: "pod_MicronutrientReport"
         },
         {
-        	podId: 23,
-        	podAccessString: "pod_WorkoutSummaryReportTssIF"
+            podId: 23,
+            podAccessString: "pod_WorkoutSummaryReportTssIF"
         },
         {
-        	podId: 24,
-        	podAccessString: "pod_TimeInPowerZonesByDate"
+            podId: 24,
+            podAccessString: "pod_TimeInPowerZonesByDate"
         },
         {
-        	podId: 25,
-        	podAccessString: "pod_TimeInPowerZonesByWeek"
+            podId: 25,
+            podAccessString: "pod_TimeInPowerZonesByWeek"
         },
         {
-        	podId: 26,
-        	podAccessString: "pod_TimeInSpeedZonesByDate"
+            podId: 26,
+            podAccessString: "pod_TimeInSpeedZonesByDate"
         },
         {
-        	podId: 27,
-        	podAccessString: "pod_TimeInSpeedZonesByWeek"
+            podId: 27,
+            podAccessString: "pod_TimeInSpeedZonesByWeek"
         },
         {
-        	podId: 28,
-        	podAccessString: "pod_meanMaxHeartRateBests"
+            podId: 28,
+            podAccessString: "pod_meanMaxHeartRateBests"
         },
         {
-        	podId: 29,
-        	podAccessString: "pod_meanMaxCadenceBests"
+            podId: 29,
+            podAccessString: "pod_meanMaxCadenceBests"
         },
         {
-        	podId: 30,
-        	podAccessString: "pod_meanMaxSpeedBests"
+            podId: 30,
+            podAccessString: "pod_meanMaxSpeedBests"
         },
         {
-        	podId: 31,
-        	podAccessString: "pod_meanMaxPaceBests"
+            podId: 31,
+            podAccessString: "pod_meanMaxPaceBests"
         },
         {
-        	podId: 32,
-        	podAccessString: "pod_PerformanceManagerChart"
+            podId: 32,
+            podAccessString: "pod_PerformanceManagerChart"
         },
         {
-        	podId: 33,
-        	podAccessString: "pod_PowerProfileChart"
+            podId: 33,
+            podAccessString: "pod_PowerProfileChart"
         },
         {
-        	podId: 34,
-        	podAccessString: "pod_WeatherForecast"
+            podId: 34,
+            podAccessString: "pod_WeatherForecast"
         },
         {
-        	podId: 35,
-        	podAccessString: "pod_WKOSummary"
+            podId: 35,
+            podAccessString: "pod_WKOSummary"
         },
         {
-        	podId: 36,
-        	podAccessString: "pod_meanMaxDistanceBests"
+            podId: 36,
+            podAccessString: "pod_meanMaxDistanceBests"
         }
     ];
 
