50119610dba5141d75b2409efbb03d9d034a2de5
Micah Condon <mcondon@peaksware.com>
2013-10-15 16:46:00 -0600

    US1638 fixed tests

--- test/specs/unit_tests/expando/views/graphToolbarView.spec.js
+++ test/specs/unit_tests/expando/views/graphToolbarView.spec.js
@@ -10,15 +10,24 @@ function($, TP, moment, theMarsApp, GraphToolbarView)
 {
     describe("GraphToolbarView", function()
     {
+        var dataParser;
+        var stateModel;
+
+        beforeEach(function()
+        {
+            dataParser = jasmine.createSpy("Data Parser");
+            stateModel = new TP.Model();
+        });
+
         it("Should have a valid contructor", function()
         {
             expect(GraphToolbarView).toBeDefined();
-            expect(function() { new GraphToolbarView({ dataParser: null }); }).not.toThrow();
+            expect(function() { new GraphToolbarView({ dataParser: dataParser, stateModel: stateModel }); }).not.toThrow();
         });
 
         it("Should trigger an event when the slider bar changes value", function()
         {
-            var view = new GraphToolbarView({ dataParser: null });
+            var view = new GraphToolbarView({ dataParser: dataParser, stateModel: stateModel });
             view.onRender = function()
             {
             };
@@ -47,7 +56,7 @@ function($, TP, moment, theMarsApp, GraphToolbarView)
         });
         it("Should correctly serialize data for distance units", null,function()
         {
-            var view = new GraphToolbarView({ dataParser: null, model: new TP.model({workoutTypeValueId: 1}) });
+            var view = new GraphToolbarView({ dataParser: dataParser, stateModel: stateModel, model: new TP.model({workoutTypeValueId: 1}) });
             expect(view.serializeData().speedLabel).toBe("yds");
 
             view.model.set({workoutTypeValueId: 3});
