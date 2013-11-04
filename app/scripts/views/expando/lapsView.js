d068973687792147b156abf9951967521c114fde
Micah Condon <mcondon@peaksware.com>
2013-11-04 10:12:06 -0700

    US1823 made lap editable on click name or time. refactored. added unit tests.

--- app/scripts/views/expando/lapsView.js
+++ app/scripts/views/expando/lapsView.js
@@ -2,218 +2,24 @@
 [
     "underscore",
     "setImmediate",
-    "jqueryui/widget",
     "jquerySelectBox",
 
     "TP",
-    "framework/tooltips",
-    "utilities/workout/formatPeakTime",
-    "utilities/workout/formatPeakDistance",
     "models/workoutStatsForRange",
-    "views/userConfirmationView",
+    "views/expando/lapView",
 
-    "hbs!templates/views/expando/lapTemplate",
-    "hbs!templates/views/expando/lapsTemplate",
-    "hbs!templates/views/confirmationViews/deleteLapTemplate"
+    "hbs!templates/views/expando/lapsTemplate"
 ],
 function(
     _,
     setImmediate,
-    jqueryUiWidget,
     jquerySelectBox,
     TP,
-    ToolTips,
-    formatPeakTime,
-    formatPeakDistance,
     WorkoutStatsForRange,
-    UserConfirmationView,
-    lapTemplate,
-    lapsTemplate,
-    deleteLapTemplate
+    LapView,
+    lapsTemplate
 )
 {
-
-    var LapView = TP.ItemView.extend(
-    {
-
-        tagName: "li",
-        className: "lap clickable",
-
-        template:
-        {
-            type: "handlebars",
-            template: lapTemplate,
-        },
-
-        modelEvents:
-        {
-            "change:isFocused": "_onFocusedChange",
-            "change:isSelected": "_onSelectedChange",
-            "change:name": "_onNameChange",
-            "change:isLap change:totalTime change:isCut": "render"
-        },
-
-        events:
-        {
-            "click .editLapName": "_onClickToEdit", // this comes before onClick, so onClick hasn't already changed our state on the first click
-            "click :not(input[type=checkbox])": "_onClick",
-            "click .delete": "_onClickDelete",
-            "click .add": "_onClickAdd",
-            "change input[type=checkbox]": "_onCheckboxChange"
-        },
-
-        initialize: function(options)
-        {
-            this.stateModel = options.stateModel;
-        },
-
-        onRender: function()
-        {
-            this._onFocusedChange();
-            this._onSelectedChange();
-
-            if(this.model.get("isEditing"))
-            {
-                this._startEditing();
-            }
-        },
-
-        serializeData: function()
-        {
-            var data = LapView.__super__.serializeData.apply(this, arguments);
-            if(data.formattedValue)
-            {
-                data.name += " (" + data.formattedValue + ")";
-            }
-            return data;
-        },
-
-        _onNameChange: function()
-        {
-            this.render();
-
-            if(this.model.get("isFocused"))
-            {
-                this.stateModel.trigger('change:primaryRange', this.stateModel, this.model);
-            }
-        },
-
-        _onFocusedChange: function()
-        {
-            var focused = !!this.model.get("isFocused");
-            this.$el.toggleClass("highlight", focused);
-        },
-
-        _onSelectedChange: function()
-        {
-            this.$("input").attr("checked", !!this.model.get("isSelected"));
-        },
-
-        _onClickToEdit: function(e)
-        {
-            if(this.model.get("isLap") && this.model.get("isFocused") && !this.model.get("isEditing"))
-            {
-                e.preventDefault();
-                this._startEditing();
-            }
-        },
-
-        _onClick: function(e)
-        {
-            this.stateModel.set("primaryRange", this.model);
-        },
-
-        _startEditing: function()
-        {
-            if(this.model.get("isLap"))
-            {
-
-                this.model.set("isEditing", true);
-                var $container = this.$(".editLapName");
-                var $input = $('<input type="text"/>');
-                $input.val(this.model.get("name"));
-
-                ToolTips.disableTooltips();
-
-                $container.empty().append($input);
-                this.$el.addClass('editing');
-                $input.on("blur enter", _.bind(this._stopEditing, this));
-                $input.on("cancel", _.bind(this._cancelEditing, this));
-                $input.focus().select();
-            }
-        },
-
-        _cancelEditing: function(e)
-        {
-            this._stopEditing(e, true);
-        },
-
-        _stopEditing: function(e, cancel)
-        {
-            e.preventDefault();
-            if(this.model.get("isLap"))
-            {
-
-                this.model.set("isEditing", false);
-
-                var $input = this.$("input[type=text]");
-
-                if($input && $input.length)
-                {
-                    var name = $input.val();
-                    this.$("input[type=text]").off("blur enter");
-                    this.$(".editLapName").empty().text(this.model.get("name"));
-                    this.$el.removeClass("editing");
-                    ToolTips.enableTooltips();
-
-                    if(!cancel)
-                    {
-                        this.model.set("name", name);
-                    }
-                }
-
-            }
-        },
-
-        _onClickDelete: function(e)
-        {
-            var deleteConfirmationView = new UserConfirmationView({ template: deleteLapTemplate });
-            deleteConfirmationView.render();
-            this.listenTo(deleteConfirmationView, "userConfirmed", _.bind(this._deleteLap, this));
-        },
-
-        _onClickAdd: function(e)
-        {
-            this.model.set({temporary: false, isLap: true, isEditing: true});
-        },
-
-        _deleteLap: function()
-        {
-            if(this.model.get("isFocused"))
-            {
-                this.stateModel.set("primaryRange", null);
-            }
-            this.model.set({ deleted: true, isFocused: false });
-            this.$el.addClass('deleted');
-            this.model.trigger('lap:markedAsDeleted', this.model);
-            this.undelegateEvents();
-            this.$('input').attr('disabled', true);
-            this.stateModel.get("ranges").remove(this.model);
-        },
-
-        _onCheckboxChange: function(e)
-        {
-            if(this.$("input").is(":checked"))
-            {
-                this.stateModel.get("ranges").add(this.model);
-            }
-            else
-            {
-                this.stateModel.get("ranges").remove(this.model);
-            }
-        }
-
-    });
 
     var LapsView = TP.CompositeView.extend(
     {
