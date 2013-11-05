7b0a17a75e3cad3a4ae2c6dd2a52ea07caffbe3f
Micah Condon <mcondon@peaksware.com>
2013-11-05 16:28:12 -0700

    US1824 updated test to match view changes

--- test/specs/bdd_tests/expando/expandoStats.spec.js
+++ test/specs/bdd_tests/expando/expandoStats.spec.js
@@ -176,7 +176,7 @@ function(
                     expect(expandoStatsText).not.toContain("Lap #1");
                     expect(expandoStatsText).not.toContain("TSS --");
 
-                    $body.find(".workoutQuickView .expandoLapsRegion .lap:first .editLapName").trigger("click");
+                    $body.find(".workoutQuickView .expandoLapsRegion .lap:first").trigger("click");
                     expandoStatsText = $body.find(".workoutQuickView .expandoStatsRegion").text().replace(/\n|\r/g," ").replace(/ +/g," ");
 
                     expect(expandoStatsText).not.toContain("Entire Workout");
