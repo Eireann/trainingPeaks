75217c077af0168ebfcd19283c1223ce1d051d3e
Ryan Taylor <rtaylor@peaksware.com>
2013-11-12 16:42:00 -0700

    US1777 - Swapped upgrade content so that clicking on a future date triggers the relevant upgrade message.

--- app/scripts/shared/data/userUpgradeViewSlides.js
+++ app/scripts/shared/data/userUpgradeViewSlides.js
@@ -11,14 +11,14 @@ function(
         {
             id: "slide1",
             image: "images/assets/slide1.png",
-            title: "Advanced Scheduling Tools",
-            html: "<p>Premium members can schedule future workouts and meals including:</p><ul><li>Copy and paste workouts</li><li>Drag and drop workouts into the future</li><li>Sync calendar with Outlook, Google and iCal</li></ul>"
+            title: "Performance Manager",
+            html: "<p>The Performance Manager Chart allows you to view your Acute Training Load (ATL), Chronic Training Load (CTL), Training Stress Balance (TSB) , Training Stress Score (TSS) and Intensity Factor (IF) side by side.</p>"
         },
         {
             id: "slide2",
             image: "images/assets/slide2.png",
-            title: "Performance Manager",
-            html: "<p>The Performance Manager Chart allows you to view your Acute Training Load (ATL), Chronic Training Load (CTL), Training Stress Balance (TSB) , Training Stress Score (TSS) and Intensity Factor (IF) side by side.</p>"
+            title: "Advanced Scheduling Tools",
+            html: "<p>Premium members can schedule future workouts and meals including:</p><ul><li>Copy and paste workouts</li><li>Drag and drop workouts into the future</li><li>Sync calendar with Outlook, Google and iCal</li></ul>"
         }
     ];
 
