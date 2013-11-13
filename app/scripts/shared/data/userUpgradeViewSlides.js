34f7860637926ae99be675bf9a5b6f358b342a89
Ryan Taylor <rtaylor@peaksware.com>
2013-11-13 10:55:07 -0700

    US1777-Image directory fix and css changes

--- app/scripts/shared/data/userUpgradeViewSlides.js
+++ app/scripts/shared/data/userUpgradeViewSlides.js
@@ -10,37 +10,37 @@ function(
     [
         {
             id: "slide1",
-            image: "images/assets/userUpgradeSlides/advanced-scheduling.jpg",
+            image: "assets/images/userUpgradeSlides/advanced-scheduling.jpg",
             title: "Advanced Scheduling Tools",
             html: "<p>Premium members can schedule future workouts and meals including:</p><ul><li>Copy and paste workouts</li><li>Drag and drop workouts into the future</li><li>Sync calendar with Outlook, Google and iCal</li></ul>"
         },
         {
             id: "slide2",
-            image: "images/assets/userUpgradeSlides/performance-manager.jpg",
+            image: "assets/images/userUpgradeSlides/performance-manager.jpg",
             title: "Performance Manager",
             html: "<p>The Performance Manager Chart allows you to view your Acute Training Load (ATL), Chronic Training Load (CTL), Training Stress Balance (TSB) , Training Stress Score (TSS) and Intensity Factor (IF) side by side.</p>"
         },
         {
             id: "slide3",
-            image: "images/assets/userUpgradeSlides/enhanced-analysis.jpg",
+            image: "assets/images/userUpgradeSlides/enhanced-analysis.jpg",
             title: "Enhanced Analysis",
             html: "<p>Access to more than 30 highly customizable charts to analyze your training including:</p><ul><li>Distance and Duration</li><li>Laps and Splits</li><li>Time in Zones</li><li>Metrics Over Time</li></ul>"
         },
         {
             id: "slide4",
-            image: "images/assets/userUpgradeSlides/map-and-graph.jpg",
+            image: "assets/images/userUpgradeSlides/map-and-graph.jpg",
             title: "Detailed Maps & Graphs",
             html: "<p>Premium members can leverage powerful map and graph features to optimize training.</p><ul><li>View map and workout data simultaneously in multiple windows</li><li>Zoom into any range on the graph</li><li>View and edit data by laps, splits or location</li><li>Isolate individual data types for increased visibility</li></ul>"
         },
         {
             id: "slide5",
-            image: "images/assets/userUpgradeSlides/data-editing.jpg",
+            image: "assets/images/userUpgradeSlides/data-editing.jpg",
             title: "Data Editing",
             html: "<p>Premium members can can leverage complete data editing tools to enable the most accurate representation of their training efforts.</p><ul><li>Remove bad or corrupt device data</li><li>Correct spikes</li><li>Cut and crop workouts</li><li>Edit and create custom ranges</li></ul>"
         },
         {
             id: "slide6",
-            image: "images/assets/userUpgradeSlides/elevation-correction.jpg",
+            image: "assets/images/userUpgradeSlides/elevation-correction.jpg",
             title: "Elevation Correction",
             html: "<p>Using Elevation Correction, you can adjust elevation data based on hundreds of gigabytes of terrain information in our Ground Control™ database resulting in more accurate data.</p>"
         }
