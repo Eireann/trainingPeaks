c62d2749616a001e9c7b31e193c7261ca9b96aae
Bernardo Fanti <bfanti@peaksware.com>
2013-11-14 10:49:49 -0700

    US1947 Re-enable analytics and let GA set proper domain

--- app/scripts/framework/analytics.js
+++ app/scripts/framework/analytics.js
@@ -7,7 +7,7 @@ function (userTypeUtilities)
     return function ()
     {
         // check whether the mars app is defined and google analytics is loaded
-        if(typeof theMarsApp === "undefined" || typeof window.ga !== "undefined")
+        if(typeof theMarsApp === "undefined" || typeof window.ga === "undefined")
             return;
 
         if (arguments.length === 2 && typeof arguments[1] === "object" && arguments[1].hitType === "event")
