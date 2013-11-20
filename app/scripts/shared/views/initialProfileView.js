031d9bfbdbc7b5258fee1bac7406771960b1176f
Hargobind Khalsa <hargobind@quickleft.com>
2013-11-20 16:11:18 -0700

    US1510 Clean up input and add validations to profile view

--- app/scripts/shared/views/initialProfileView.js
+++ app/scripts/shared/views/initialProfileView.js
@@ -18,6 +18,8 @@ function(
     initialProfileTemplate
 )
 {
+    // HR: 137 - 195
+    // Swim Pace: Max: 25:21 / 100m / 1km
 
     var InitialProfileView = TP.ItemView.extend(
     {
@@ -48,12 +50,20 @@ function(
 
         initialize: function()
         {
+            var birthday = moment(theMarsApp.user.get("birthday"));
             this.model = new TP.Model({
                 language: theMarsApp.user.get("language") || "en-us",
                 unitPreference: theMarsApp.user.get("units") || 1,
                 country: theMarsApp.user.get("country") || "US",
-                swimUnits: theMarsApp.user.getUnitsBySportType(1),
-                runUnits: "pace"
+                timeZone: theMarsApp.user.get("timeZone"),
+                birthdayMonth: birthday && (birthday.month() + 1),
+                birthdayYear: birthday && birthday.year(),
+                runUnits: "pace",
+                swimUnits: 1,
+                thresholdPower: 200,
+                thresholdHeartRate: 160,
+                runPaceSpeed: 2.68224,
+                swimPace: 0.762
             });
         },
 
@@ -119,11 +129,25 @@ function(
 
             event.preventDefault();
 
+            if(this._validate())
+            {
+                this.$("input[type=submit]").attr("disabled", true);
+                this._saveAndClose().fail(function()
+                {
+                    self._showError("Failed to save profile");
+                    self.$("input[type=submit]").attr("disabled", false);
+                });
+            }
+        },
+
+        _saveAndClose: function()
+        {
+            var self = this;
             var userPromise = theMarsApp.user.save(
             {
                 language: this.model.get("language"),
                 units: this.model.get("unitPreference"),
-                birthday: moment("1900-01-01").year(this.model.get("birthdayYear") || 0).month(this.model.get("birthdayMonth") || 0).format("YYYY-MM-DD"),
+                birthday: moment("1900-01-01").year(this.model.get("birthdayYear") || 0).month(this.model.get("birthdayMonth") - 1 || 0).format("YYYY-MM-DD"),
                 timeZone: this.model.get("timeZone"),
                 country: this.model.get("country")
             });
@@ -142,14 +166,10 @@ function(
                 }
             });
 
-            $.when(profilePromise, userPromise).then(function()
+            return $.when(profilePromise, userPromise).then(function()
             {
                 theMarsApp.calendarManager.reset();
                 self.close();
-            },
-            function()
-            {
-                alert("Failed to save");
             });
         },
 
@@ -161,6 +181,26 @@ function(
             this.$(".weightUnits").text(TP.utils.units.getUnitsLabel("kg"));
             this.$("label[for=runUnitsPace]").text(TP.utils.units.getUnitsLabel("pace", 3));
             this.$("label[for=runUnitsSpeed]").text(TP.utils.units.getUnitsLabel("speed", 3));
+        },
+
+        _validate: function()
+        {
+            var errors = FormUtility.validate(this.$el, _.defaults(this.formUtilityOptions) );
+
+            if(errors.length === 0)
+            {
+                return true;
+            }
+            else
+            {
+                this._showError("Errors:\n" + errors.join("\n"));
+                return false;
+            }
+        },
+
+        _showError: function(message)
+        {
+            alert(message);
         }
 
     });
