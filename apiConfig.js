62287294fc6e54444067644419e2bc3d4c03cb49
Micah Condon <mcondon@peaksware.com>
2013-12-05 16:30:48 -0700

    DE825 updated plan store access logic and moved link url from html template to config

--- apiConfig.js
+++ apiConfig.js
@@ -15,6 +15,7 @@
 
         coachUpgradeURL: "{HOME_ROOT}/account-professional-edition.aspx",
         upgradeURL: "{HOME_ROOT}/create-account-personal-edition.aspx?login=true&utm_source=tpflex&utm_medium=trigger&utm_content=premiumfeature&utm_campaign=put",
-        helpURL: "{HELP_URL}"
+        helpURL: "{HELP_URL}",
+        trainingPlanStoreUrl: "{HOME_ROOT}/training-and-nutrition-plans.aspx"
     };
 })();
