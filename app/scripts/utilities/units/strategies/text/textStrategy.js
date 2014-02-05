0befac1e394273060fd31a9d582cad18151a3f16
Micah Condon <mcondon@peaksware.com>
2014-02-05 14:22:04 -0700

    #363 empty string should be parsed as null

--- app/scripts/utilities/units/strategies/text/textStrategy.js
+++ app/scripts/utilities/units/strategies/text/textStrategy.js
@@ -12,29 +12,34 @@ define(
     */
     return DefaultUnitsStrategy.extend({
 
-        // converts LF \n or CR \r to CRLF \r\n for proper whitespace display in browser
         formatValue: function(value)
         {
 
             // if the input is null or empty string, return empty string or other default value
-            if(!_.isString(value))
+            if(!_.isString(value) || this._isEmptyString(value))
             {
                 return this._getDefaultValueForFormat();
             }           
 
+            // converts LF \n or CR \r to CRLF \r\n for proper whitespace display in browser
             return value.replace(/\r\n/g, "\n").replace(/\r/g,"\n").replace(/\n/g, "\r\n");
         },
 
         
-        // converts CRLF \r\n or LF \n to CR \r  because FLEX WANTS \r, not \n, don't ask me why ...
         parseValue: function(value)
         {
-            if(!_.isString(value))
+            if(!_.isString(value) || this._isEmptyString(value))
             {
                 return this._getDefaultValueForParse();
             }
 
+            // converts CRLF \r\n or LF \n to CR \r  because FLEX WANTS \r, not \n, don't ask me why ...
             return value.replace(/\r\n/g, "\r").replace(/\n/g, "\r");
+        },
+
+        _isEmptyString: function(value)
+        {
+            return value.replace(/\s/g,"") === "";
         }
 
     });
