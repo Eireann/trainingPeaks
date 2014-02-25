ea669766143ba1a3b1b5459e66161c24d87fa876
Laurent <ltherond@peaksware.com>
2014-02-25 14:25:32 -0700

    US371WorkoutPrescription: Fix scroll to behavior.

--- app/scripts/views/quickView/summaryView.js
+++ app/scripts/views/quickView/summaryView.js
@@ -128,7 +128,7 @@ function (
 
             $container.animate(
                 {
-                    scrollTop: $target.position().top + $container.scrollTop()
+                    scrollTop: $target.position().top - $container.position().top
                 },
                 {
                     done: function() { self.scrolling = false; }
