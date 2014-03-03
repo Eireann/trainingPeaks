7465e06b5dacf84393cc03e0d1f7af18bbfa3b8d
Laurent <ltherond@peaksware.com>
2014-03-03 10:58:16 -0700

    US371WorkoutPrescription: Extract notes from exercise instructions to present them in a section with more real estate. Override label of 'Percent HR' to avoid overflowing alloted column space. Apply post-processing to actual HR/Speed/Power zones to resolve zone numbers to zone ranges.

--- app/scripts/views/workout/exerciseSetView.js
+++ app/scripts/views/workout/exerciseSetView.js
@@ -29,39 +29,88 @@ function (
             data.index = this.options.index + 1;
 
             var activeOrRest = _.find(data.instructions, { name: "Active or Rest" });
-
             if (activeOrRest)
                 data.active = activeOrRest.planValue;
 
+            var notes = _.find(data.instructions, { name: "Notes" });
+            if (notes)
+            {
+                data.planNote = notes.planValue;
+                data.actualNote = notes.actualValue;
+            }
+
             data.instructions = _.reject(data.instructions, { name: "Active or Rest" });
+            data.instructions = _.reject(data.instructions, { name: "Notes" });
 
             _.each(data.instructions, function(instruction)
             {
-                // 9 = "HR Zone"
-                if (instruction.type === 9)
+                instruction.name = TP.utils.translate(instruction.name);
+
+                // 24 = "Percent HR"
+                if (instruction.type === 24)
                 {
-                    var hrZone = theMarsApp.user.getAthleteSettings().get("heartRateZones.0.zones." + (instruction.planValue - 1));
-                    if (hrZone)
+                    // Real estate for this label is very limited, so override its content.
+                    instruction.name = TP.utils.translate("%max HR");
+                    instruction.planUnitsSpecified = false;
+                }
+                // 9 = "HR Zone"
+                else if (instruction.type === 9)
+                {
+                    if (instruction.planValueSpecified)
                     {
-                        instruction.planValue = hrZone.minimum + " - " + hrZone.maximum;
+                        var planHrZone = theMarsApp.user.getAthleteSettings().get("heartRateZones.0.zones." + (instruction.planValue - 1));
+                        if (planHrZone)
+                        {
+                            instruction.planValue = planHrZone.minimum + " - " + planHrZone.maximum;
+                        }
+                    }
+                    if (instruction.actualValueSpecified)
+                    {
+                        var actualHrZone = theMarsApp.user.getAthleteSettings().get("heartRateZones.0.zones." + (instruction.actualValue - 1));
+                        if (actualHrZone)
+                        {
+                            instruction.actualValue = actualHrZone.minimum + " - " + actualHrZone.maximum;
+                        }
                     }
                 }
                 // 26 = "Speed Zone"
                 else if (instruction.type === 26)
                 {
-                    var speedZone = theMarsApp.user.getAthleteSettings().get("speedZones.0.zones." + (instruction.planValue - 1));
-                    if (speedZone)
+                    if (instruction.planValueSpecified)
                     {
-                        instruction.planValue = TP.utils.conversion.formatUnitsValue("pace", speedZone.minimum) + " - " + TP.utils.conversion.formatUnitsValue("pace", speedZone.maximum);
+                        var planSpeedZone = theMarsApp.user.getAthleteSettings().get("speedZones.0.zones." + (instruction.planValue - 1));
+                        if (planSpeedZone)
+                        {
+                            instruction.planValue = TP.utils.conversion.formatUnitsValue("pace", planSpeedZone.minimum) + " - " + TP.utils.conversion.formatUnitsValue("pace", planSpeedZone.maximum);
+                        }
+                    }
+                    if (instruction.actualValueSpecified)
+                    {
+                        var actualSpeedZone = theMarsApp.user.getAthleteSettings().get("speedZones.0.zones." + (instruction.actualValue - 1));
+                        if (actualSpeedZone)
+                        {
+                            instruction.actualValue = TP.utils.conversion.formatUnitsValue("pace", actualSpeedZone.minimum) + " - " + TP.utils.conversion.formatUnitsValue("pace", actualSpeedZone.maximum);
+                        }
                     }
                 }
                 // 11 = "Power Zone"
                 else if (instruction.type === 11)
                 {
-                    var powerZone = theMarsApp.user.getAthleteSettings().get("powerZones.0.zones." + (instruction.planValue - 1));
-                    if (powerZone)
+                    if (instruction.planValueSpecified)
                     {
-                        instruction.planValue = TP.utils.conversion.formatUnitsValue("power", powerZone.minimum) + " - " + TP.utils.conversion.formatUnitsValue("power", powerZone.maximum);
+                        var planPowerZone = theMarsApp.user.getAthleteSettings().get("powerZones.0.zones." + (instruction.planValue - 1));
+                        if (planPowerZone)
+                        {
+                            instruction.planValue = TP.utils.conversion.formatUnitsValue("power", planPowerZone.minimum) + " - " + TP.utils.conversion.formatUnitsValue("power", planPowerZone.maximum);
+                        }
+                    }
+                    if (instruction.actualValueSpecified)
+                    {
+                        var actualPowerZone = theMarsApp.user.getAthleteSettings().get("powerZones.0.zones." + (instruction.actualValue - 1));
+                        if (actualPowerZone)
+                        {
+                            instruction.actualValue = TP.utils.conversion.formatUnitsValue("power", actualPowerZone.minimum) + " - " + TP.utils.conversion.formatUnitsValue("power", actualPowerZone.maximum);
+                        }
                     }
                 }
 
